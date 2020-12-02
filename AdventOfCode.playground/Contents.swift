9a4d041cc5afab036961ae04c1eeab6185181f00
Ben Lings <ben.lings@gmail.com>
2020-12-02 21:24:54 +0000

    Day 2 part 2
    Didn't think it was worth abstracting the policy from the storage of the elements

--- AdventOfCode.playground/Contents.swift
+++ AdventOfCode.playground/Contents.swift
@@ -1,4 +1,8 @@
 
 import AdventOfCode_Sources
 
+day1_1()
+day1_2()
+
 day2_1()
+day2_2()
--- AdventOfCode.playground/Sources/day2.swift
+++ AdventOfCode.playground/Sources/day2.swift
@@ -1,15 +1,26 @@
 import Foundation
 
 public struct Policy {
-    public var limit: ClosedRange<Int>
+    public var lower: Int
+    public var upper: Int
+    public var limit: ClosedRange<Int> {
+        lower...upper
+    }
     public var character: Character
 
-    public func conforms(password: String) -> Bool {
+    public func conforms1(password: String) -> Bool {
         limit.contains(password.filter { $0 == character }.count)
+    }
+
+    public func conforms2(password: String) -> Bool {
+        let characters = Array(password)
+        let pos1 = characters[lower - 1] == character
+        let pos2 = characters[upper - 1] == character
+        return pos1 != pos2
     }
 }
 
-
+// eg. "1-3 a: abcde"
 public func parseRow(_ row: String) -> (Policy, String) {
     let scanner = Scanner(string: row)
     let lower = scanner.scanInt()!
@@ -18,14 +29,24 @@ public func parseRow(_ row: String) -> (Policy, String) {
     let character = scanner.scanCharacter()!
     _ = scanner.scanString(":")!
     let password = scanner.scanUpToCharacters(from: .whitespacesAndNewlines)!
-    return (Policy(limit: lower...upper, character: character), password)
+    return (Policy(lower: lower, upper: upper, character: character), password)
 }
 
 public func day2_1() {
     let count = readLines("day2.txt")
         .map(parseRow)
         .filter { (policy, password) in
-            policy.conforms(password: password)
+            policy.conforms1(password: password)
+        }
+        .count
+    print("\(count)")
+}
+
+public func day2_2() {
+    let count = readLines("day2.txt")
+        .map(parseRow)
+        .filter { (policy, password) in
+            policy.conforms2(password: password)
         }
         .count
     print("\(count)")

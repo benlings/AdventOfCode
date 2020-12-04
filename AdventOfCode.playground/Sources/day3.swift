0d3cd89ec382a55f752d021e9481158d44f59ce8
Ben Lings <ben.lings@gmail.com>
2020-12-04 06:52:37 +0000

    Extract extension for getting every nth element

--- AdventOfCode.playground/Sources/day3.swift
+++ AdventOfCode.playground/Sources/day3.swift
@@ -1,12 +1,18 @@
 import Foundation
 
+public extension Collection {
+    func every(nth: Int) -> [Element] {
+        self.enumerated()
+            .filter { idx, _ in idx % nth == 0 }
+            .map { $1 }
+    }
+}
+
 public func count(trees: [[Int]], right: Int, down: Int) -> Int {
     let rowLength = trees.first!.count
     let indices = (0...).lazy.map { ($0 * right) % rowLength }
-    let skippedTrees = trees.enumerated()
-        .filter { idx, _ in idx % down == 0 }
-        .map { $1 }
-    let count = zip(indices, skippedTrees)
+    let count = zip(indices,
+                    trees.every(nth: down))
         .map { $1[$0] }
         .reduce(0, +)
     return count

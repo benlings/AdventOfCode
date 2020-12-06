314c2a09c253b7bd9bb98d923db07af557bf2d8f
Ben Lings <ben.lings@gmail.com>
2020-12-06 08:32:54 +0000

    Add swift-algorithms dependency

--- Package.swift
+++ Package.swift
@@ -18,13 +18,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-algorithms", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AdventCore",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms"),
+            ]),
         .target(
             name: "AoC2020",
             dependencies: ["AdventCore"],

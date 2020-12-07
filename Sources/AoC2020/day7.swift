9116dd16fcdecc3e0ee430ba3aac0fbfaa7e70d6
Ben Lings <ben.lings@gmail.com>
2020-12-07 09:52:15 +0000

    Day 7 part 1
    Way too slow

--- Sources/AoC2020/day7.swift
+++ Sources/AoC2020/day7.swift
@@ -5,9 +5,19 @@ public struct LuggageBag : Hashable, Equatable {
     var bagDescription: String
 }
 
+extension LuggageBag : ExpressibleByStringLiteral {
+    public init(stringLiteral value: StringLiteralType) {
+        self = .init(bagDescription: value)
+    }
+}
+
 public struct LuggageRule : Equatable {
     var bag: LuggageBag
     var contents = Set<LuggageBag>() // FIXME add count
+
+    func transitiveContents(rules: [LuggageRule]) -> Set<LuggageBag> {
+        contents.union(rules.filter { contents.contains($0.bag) }.flatMap { $0.transitiveContents(rules: rules) })
+    }
 }
 
 extension Scanner {
@@ -35,4 +45,30 @@ public extension LuggageRule {
             _ = scanner.scanString(",") ?? scanner.scanString(".")
         }
     }
+}
+
+public struct LuggageProcessor {
+    var rules: [LuggageRule]
+
+    public func bags(thatCanContain bag: LuggageBag) -> Set<LuggageBag> {
+        var containingBags = Set<LuggageBag>()
+        for rule in rules {
+            if rule.transitiveContents(rules: rules).contains(bag) {
+                containingBags.insert(rule.bag)
+            }
+        }
+        return containingBags
+    }
+}
+
+extension LuggageProcessor {
+    public init(rulesDescription: String) {
+        self = .init(rules: rulesDescription.lines().compactMap(LuggageRule.init))
+    }
+}
+
+fileprivate let day5_input = Bundle.module.text(named: "day7")
+
+func day7_1() -> Int {
+    LuggageProcessor(rulesDescription: day5_input).bags(thatCanContain: "shiny gold").count
 }
--- Tests/AoC2020Tests/day7.swift
+++ Tests/AoC2020Tests/day7.swift
@@ -29,8 +29,25 @@ final class Day7Tests: XCTestCase {
         XCTAssertEqual(expected, LuggageRule(input))
     }
 
+    func testBagsThatContain() {
+        let input = """
+        light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags.
+        """
+        let processor = LuggageProcessor(rulesDescription: input)
+        let expectedBags = ["bright white", "muted yellow", "dark orange", "light red"] as Set<LuggageBag>
+        XCTAssertEqual(expectedBags, processor.bags(thatCanContain: "shiny gold"))
+    }
+
     func testPart1() {
-//        XCTAssertEqual(day7_1(), 0)
+        XCTAssertEqual(day7_1(), 252)
     }
 
     func testPart2() {

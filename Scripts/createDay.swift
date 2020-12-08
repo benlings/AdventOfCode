ed8b9f984d58f7c87381526459d3805f270ceae5
Ben Lings <ben.lings@gmail.com>
2020-12-08 22:30:35 +0000

    Add script to create empty files

---
+++ Scripts/createDay.swift
@@ -0,0 +1,46 @@
+#!/usr/bin/swift
+
+import Foundation
+
+let year = CommandLine.arguments[1]
+let day = CommandLine.arguments[2]
+
+print("Creating files for AoC \(year), day \(day)")
+
+let source = """
+import Foundation
+import AdventCore
+
+public func day\(day)_1() -> Int {
+    0
+}
+
+public func day\(day)_2() -> Int {
+    0
+}
+"""
+
+let tests = """
+import XCTest
+@testable import AoC\(year)
+
+final class Day\(day)Tests: XCTestCase {
+
+    func testPart1() {
+        XCTAssertEqual(day\(day)_1(), 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day\(day)_2(), 0)
+    }
+}
+"""
+
+let sourceURL = URL(fileURLWithPath: "Sources/AoC\(year)/day\(day).swift")
+let testURL = URL(fileURLWithPath: "Tests/AoC\(year)Tests/day\(day).swift")
+let inputURL = URL(fileURLWithPath: "Sources/AoC\(year)/Resources/day\(day).txt")
+
+try? source.write(to: sourceURL, atomically: true, encoding: .utf8)
+try? tests.write(to: testURL, atomically: true, encoding: .utf8)
+try? "".write(to: inputURL, atomically: true, encoding: .utf8)
+

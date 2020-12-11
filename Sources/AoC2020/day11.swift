3b4811c0c2caca233a96e01a16d744fd553475f7
Ben Lings <ben.lings@gmail.com>
2020-12-11 12:43:33 +0000

    Add day 11 placeholder

---
+++ Sources/AoC2020/day11.swift
@@ -0,0 +1,10 @@
+import Foundation
+import AdventCore
+
+public func day11_1() -> Int {
+    0
+}
+
+public func day11_2() -> Int {
+    0
+}
---
+++ Tests/AoC2020Tests/day11.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import AoC2020
+
+final class Day11Tests: XCTestCase {
+
+    func testPart1() {
+        XCTAssertEqual(day11_1(), 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day11_2(), 0)
+    }
+}

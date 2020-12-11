fe198b6ea8b273cb54ad5f29ab3ba1c7cda519a0
Ben Lings <ben.lings@gmail.com>
2020-12-11 20:10:18 +0000

    Part 2 examples

--- Sources/AoC2020/day11.swift
+++ Sources/AoC2020/day11.swift
@@ -46,27 +46,66 @@ public struct WaitingArea : Equatable {
             (self[x + 1, y + 1] == .occupied ? 1 : 0)
     }
 
-    mutating func step() {
+    func visibleSeatFromPosition(x:Int, y:Int, directionX: Int, directionY: Int) -> Seat {
+        guard columnIndices.contains(y), rowIndices.contains(x) else {
+            return .floor
+        }
+        switch self[x + directionX, y + directionY] {
+        case .floor: return visibleSeatFromPosition(x: x + directionX, y: y + directionY,
+                                                    directionX: directionX, directionY: directionY)
+        case let seat: return seat
+        }
+    }
+
+    func visibleNeighbours(x: Int, y: Int) -> Int {
+        (visibleSeatFromPosition(x: x, y: y, directionX: -1, directionY: -1) == .occupied ? 1 : 0) +
+            (visibleSeatFromPosition(x: x, y: y, directionX: -1, directionY: 0) == .occupied ? 1 : 0) +
+            (visibleSeatFromPosition(x: x, y: y, directionX: -1, directionY: 1) == .occupied ? 1 : 0) +
+            (visibleSeatFromPosition(x: x, y: y, directionX: 0, directionY: -1) == .occupied ? 1 : 0) +
+            (visibleSeatFromPosition(x: x, y: y, directionX: 0, directionY: 1) == .occupied ? 1 : 0) +
+            (visibleSeatFromPosition(x: x, y: y, directionX: 1, directionY: -1) == .occupied ? 1 : 0) +
+            (visibleSeatFromPosition(x: x, y: y, directionX: 1, directionY: 0) == .occupied ? 1 : 0) +
+            (visibleSeatFromPosition(x: x, y: y, directionX: 1, directionY: 1) == .occupied ? 1 : 0)
+
+    }
+
+    mutating func step(strategy: Strategy = .neighbour) {
         let oldLayout = self
         for y in columnIndices {
             for x in rowIndices {
-                switch oldLayout[x, y] {
-                case .empty where oldLayout.seatedNeighbours(x: x, y: y) == 0:
-                    self[x, y] = .occupied
-                case .occupied where oldLayout.seatedNeighbours(x: x, y: y) >= 4:
-                    self[x, y] = .empty
-                default:
-                    break
+                if strategy == .neighbour {
+                    switch oldLayout[x, y] {
+                    case .empty where oldLayout.seatedNeighbours(x: x, y: y) == 0:
+                        self[x, y] = .occupied
+                    case .occupied where oldLayout.seatedNeighbours(x: x, y: y) >= 4:
+                        self[x, y] = .empty
+                    default:
+                        break
+                    }
+                } else {
+                    switch oldLayout[x, y] {
+                    case .empty where oldLayout.visibleNeighbours(x: x, y: y) == 0:
+                        self[x, y] = .occupied
+                    case .occupied where oldLayout.visibleNeighbours(x: x, y: y) >= 5:
+                        self[x, y] = .empty
+                    default:
+                        break
+                    }
+
                 }
             }
         }
     }
 
-    public mutating func findSteadyState() {
+    public enum Strategy {
+        case neighbour, visible
+    }
+
+    public mutating func findSteadyState(strategy: Strategy = .neighbour) {
         var oldLayout: Self
         repeat {
             oldLayout = self
-            self.step()
+            self.step(strategy: strategy)
         } while oldLayout != self
     }
 
--- Tests/AoC2020Tests/day11.swift
+++ Tests/AoC2020Tests/day11.swift
@@ -52,6 +52,63 @@ final class Day11Tests: XCTestCase {
         XCTAssertEqual(day11_1(), 2441)
     }
 
+    func testPart2Example() {
+        let input = """
+        L.LL.LL.LL
+        LLLLLLL.LL
+        L.L.L..L..
+        LLLL.LL.LL
+        L.LL.LL.LL
+        L.LLLLL.LL
+        ..L.L.....
+        LLLLLLLLLL
+        L.LLLLLL.L
+        L.LLLLL.LL
+        """
+        var waitingArea = WaitingArea(input)
+        waitingArea.step(strategy: .visible)
+        XCTAssertEqual(waitingArea.description, """
+            #.##.##.##
+            #######.##
+            #.#.#..#..
+            ####.##.##
+            #.##.##.##
+            #.#####.##
+            ..#.#.....
+            ##########
+            #.######.#
+            #.#####.##
+            """)
+        waitingArea.step(strategy: .visible)
+        XCTAssertEqual(waitingArea.description, """
+            #.LL.LL.L#
+            #LLLLLL.LL
+            L.L.L..L..
+            LLLL.LL.LL
+            L.LL.LL.LL
+            L.LLLLL.LL
+            ..L.L.....
+            LLLLLLLLL#
+            #.LLLLLL.L
+            #.LLLLL.L#
+            """)
+        waitingArea.findSteadyState(strategy: .visible)
+        XCTAssertEqual(waitingArea.description, """
+            #.L#.L#.L#
+            #LLLLLL.LL
+            L.L.L..#..
+            ##L#.#L.L#
+            L.L#.LL.L#
+            #.LLLL#.LL
+            ..#.L.....
+            LLL###LLL#
+            #.LLLLL#.L
+            #.L#LL#.L#
+            """)
+        XCTAssertEqual(waitingArea.occupied, 26)
+
+    }
+
     func testPart2() {
         XCTAssertEqual(day11_2(), 0)
     }

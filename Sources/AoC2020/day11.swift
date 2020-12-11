ad026529354a12c666722426bd2b3436bf64b2a3
Ben Lings <ben.lings@gmail.com>
2020-12-11 12:57:33 +0000

    Parse seat input

--- Sources/AoC2020/day11.swift
+++ Sources/AoC2020/day11.swift
@@ -1,6 +1,38 @@
 import Foundation
 import AdventCore
 
+
+public struct WaitingArea {
+
+    public enum Seat : Character {
+        case floor = "."
+        case empty = "L"
+        case occupied = "#"
+    }
+
+    var seats: [[Seat]]
+
+}
+
+public extension WaitingArea {
+    init(_ description: String) {
+        seats = description
+            .lines()
+            .map {
+                $0.compactMap(Seat.init(rawValue:))
+            }
+    }
+}
+
+extension WaitingArea : CustomStringConvertible {
+    public var description: String {
+        seats.map { row in
+            String(row.map(\.rawValue))
+        }.joined(separator: "\n")
+    }
+}
+
+
 public func day11_1() -> Int {
     0
 }
--- Tests/AoC2020Tests/day11.swift
+++ Tests/AoC2020Tests/day11.swift
@@ -3,6 +3,23 @@ import XCTest
 
 final class Day11Tests: XCTestCase {
 
+    func testPart1Example() {
+        let input = """
+        L.LL.LL.LL
+        LLLLLLL.LL
+        L.L.L..L..
+        LLLL.LL.LL
+        L.LL.LL.LL
+        L.LLLLL.LL
+        ..L.L.....
+        LLLLLLLLLL
+        L.LLLLLL.L
+        L.LLLLL.LL
+        """
+        var waitingArea = WaitingArea(input)
+        XCTAssertEqual(waitingArea.description, input)
+    }
+
     func testPart1() {
         XCTAssertEqual(day11_1(), 0)
     }

21a32421072ece90e9595ec6a1c2cdf6309530bc
Ben Lings <ben.lings@gmail.com>
2020-12-13 12:33:24 +0000

    Day 13 part 2

--- Sources/AoC2020/day13.swift
+++ Sources/AoC2020/day13.swift
@@ -1,6 +1,23 @@
 import Foundation
 import AdventCore
 
+func gcd(_ m: Int, _ n: Int) -> Int {
+    let r = m % n
+    if r != 0 {
+        return gcd(n, r)
+    } else {
+        return n
+    }
+}
+
+func lcm(_ m: Int, _ n: Int) -> Int {
+    m / gcd(m, n) * n
+}
+
+func lcm(_ values: [Int]) -> Int {
+    values.reduce { lcm($0, $1) } ?? 0
+}
+
 public struct BusTimetable {
     var earliestTimestamp: Int
     var busIds: [Int?]
@@ -21,8 +38,28 @@ public struct BusTimetable {
     public func answer() -> Int {
         let (busId, time) = findBus()
         return busId * (time - earliestTimestamp)
-
     }
+
+    public func earliestTimestampMatchingOffsets() -> Int {
+        let requiredBusIds = busIds.enumerated().compactMap { (offset, id) -> (Int, Int)? in
+            guard let busId = id else {
+                return nil
+            }
+            return (offset, busId)
+        }
+        var time = 0
+        while true {
+            let matchingBusIds = requiredBusIds.filter { (offset, busId) in
+                return (time + offset).isMultiple(of: busId)
+            }.map(\.1)
+            if matchingBusIds.count == requiredBusIds.count {
+                return time
+            }
+            time += lcm(matchingBusIds)
+        }
+    }
+
+
 }
 
 public extension BusTimetable {
@@ -40,5 +77,5 @@ public func day13_1() -> Int {
 }
 
 public func day13_2() -> Int {
-    0
+    BusTimetable(input).earliestTimestampMatchingOffsets()
 }
--- Tests/AoC2020Tests/day13.swift
+++ Tests/AoC2020Tests/day13.swift
@@ -3,12 +3,12 @@ import AoC2020
 
 final class Day13Tests: XCTestCase {
 
+    let timetable = BusTimetable("""
+    939
+    7,13,x,x,59,x,31,19
+    """)
+
     func testPart1Example() {
-        let input = """
-        939
-        7,13,x,x,59,x,31,19
-        """
-        let timetable = BusTimetable(input)
         XCTAssertEqual(timetable.answer(), 295)
     }
 
@@ -16,7 +16,11 @@ final class Day13Tests: XCTestCase {
         XCTAssertEqual(day13_1(), 6559)
     }
 
+    func testPart2Example() {
+        XCTAssertEqual(timetable.earliestTimestampMatchingOffsets(), 1068781)
+    }
+
     func testPart2() {
-        XCTAssertEqual(day13_2(), 0)
+        XCTAssertEqual(day13_2(), 626670513163231)
     }
 }

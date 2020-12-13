727719e29f68af34ba04e8c8a6cb05d3d36fad0d
Ben Lings <ben.lings@gmail.com>
2020-12-13 08:23:59 +0000

    Add files for day 13

---
+++ Sources/AoC2020/day13.swift
@@ -0,0 +1,10 @@
+import Foundation
+import AdventCore
+
+public func day13_1() -> Int {
+    0
+}
+
+public func day13_2() -> Int {
+    0
+}
---
+++ Tests/AoC2020Tests/day13.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import AoC2020
+
+final class Day13Tests: XCTestCase {
+
+    func testPart1() {
+        XCTAssertEqual(day13_1(), 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day13_2(), 0)
+    }
+}

16748019beb8628024124b777d435d0e49b148f1
Ben Lings <ben.lings@gmail.com>
2020-12-17 06:49:20 +0000

    Scaffold day 17

---
+++ Sources/AoC2020/day17.swift
@@ -0,0 +1,10 @@
+import Foundation
+import AdventCore
+
+public func day17_1() -> Int {
+    0
+}
+
+public func day17_2() -> Int {
+    0
+}
---
+++ Tests/AoC2020Tests/day17.swift
@@ -0,0 +1,13 @@
+import XCTest
+import AoC2020
+
+final class Day17Tests: XCTestCase {
+
+    func testPart1() {
+        XCTAssertEqual(day17_1(), 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day17_2(), 0)
+    }
+}

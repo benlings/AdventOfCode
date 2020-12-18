5fb014451d8abe724d54bc6870aa92b5a861670f
Ben Lings <ben.lings@gmail.com>
2020-12-18 12:52:39 +0000

    Refactor to intermediate expression tree

--- Sources/AoC2020/day18.swift
+++ Sources/AoC2020/day18.swift
@@ -1,6 +1,18 @@
 import Foundation
 import AdventCore
 
+public indirect enum Expr {
+    case number(Int)
+    case operation(Expr, Expr, (Int, Int) -> Int)
+
+    public func eval() -> Int {
+        switch self {
+        case .number(let n): return n
+        case .operation(let lhs, let rhs, let op): return op(lhs.eval(), rhs.eval())
+        }
+    }
+}
+
 extension Scanner {
 
     func peekString(_ s: String) -> Bool {
@@ -12,26 +24,27 @@ extension Scanner {
     // expr   = factor { ("*"|"+") factor }
     // factor = int | "(" expr ")"
 
-    func scanExpr() -> Int? {
+    func scanExpr() -> Expr? {
         guard var result = scanFactor() else {
             return nil
         }
         
-        while !peekString(")") && !isAtEnd {
+        // Left associative - consume these in a loop
+        while peekString("*") || peekString("+") {
             if let _ = scanString("*"),
                let rhs = scanFactor() {
-                result *= rhs
+                result = .operation(result, rhs, *)
             } else if let _ = scanString("+"),
                       let rhs = scanFactor() {
-                result += rhs
+                result = .operation(result, rhs, +)
             }
         }
         return result
     }
 
-    func scanFactor() -> Int? {
+    func scanFactor() -> Expr? {
         if let n = scanInt() {
-            return n
+            return .number(n)
         }
         if let _ = scanString("("),
            let n = scanExpr(),
@@ -45,44 +58,40 @@ extension Scanner {
     // term   = factor { "+" factor }
     // factor = int | "(" expr ")"
 
-    func scanExpr2() -> Int? {
+    func scanExpr2() -> Expr? {
         guard var result = scanTerm2() else {
             return nil
         }
 
-        while !peekString(")") && !isAtEnd {
-            let pos = currentIndex
-            if let _ = scanString("*"),
-               let rhs = scanTerm2() {
-                result *= rhs
+        // Left associative - consume in a loop
+        while scanString("*") != nil {
+            if let rhs = scanTerm2() {
+                result = .operation(result, rhs, *)
             } else {
-                currentIndex = pos
-                return result
+                return nil
             }
         }
         return result
     }
 
-    func scanTerm2() -> Int? {
+    func scanTerm2() -> Expr? {
         guard var result = scanFactor2() else {
             return nil
         }
-        while !peekString(")") && !isAtEnd {
-            let pos = currentIndex
-            if let _ = scanString("+"),
-               let rhs = scanFactor2() {
-                result += rhs
+        // Left associative - consume in a loop
+        while scanString("+") != nil {
+            if let rhs = scanFactor2() {
+                result = .operation(result, rhs, +)
             } else {
-                currentIndex = pos
-                return result
+                return nil
             }
         }
         return result
     }
 
-    func scanFactor2() -> Int? {
+    func scanFactor2() -> Expr? {
         if let n = scanInt() {
-            return n
+            return .number(n)
         }
         if let _ = scanString("("),
            let n = scanExpr2(),
@@ -95,13 +104,22 @@ extension Scanner {
 }
 
 public func evalExpr(_ input: String) -> Int {
-    let scanner = Scanner(string: input)
-    return scanner.scanExpr()!
+    return Expr(input).eval()
 }
 
 public func evalExpr2(_ input: String) -> Int {
-    let scanner = Scanner(string: input)
-    return scanner.scanExpr2()!
+    return Expr(input, v2: true).eval()
+}
+
+public extension Expr {
+    init(_ description: String, v2: Bool = false) {
+        let scanner = Scanner(string: description)
+        if v2 {
+            self = scanner.scanExpr2()!
+        } else {
+            self = scanner.scanExpr()!
+        }
+    }
 }
 
 fileprivate let input = Bundle.module.text(named: "day18")

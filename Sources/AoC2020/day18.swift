ce2b0c1a313ed7b6dd89c9af1d60ef403e73a2aa
Ben Lings <ben.lings@gmail.com>
2020-12-18 09:43:00 +0000

    Day 18 part 1

--- Sources/AoC2020/day18.swift
+++ Sources/AoC2020/day18.swift
@@ -1,8 +1,54 @@
 import Foundation
 import AdventCore
 
+extension Scanner {
+
+    func peekString(_ s: String) -> Bool {
+        let i = currentIndex
+        defer { currentIndex = i }
+        return scanString(s) != nil
+    }
+
+    func scanExpr() -> Int? {
+        guard var result = scanFactor() else {
+            return nil
+        }
+        
+        while !peekString(")") && !isAtEnd {
+            if let _ = scanString("*"),
+               let rhs = scanFactor() {
+                result *= rhs
+            } else if let _ = scanString("+"),
+                      let rhs = scanFactor() {
+                result += rhs
+            }
+        }
+        return result
+    }
+
+    func scanFactor() -> Int? {
+        if let n = scanInt() {
+            return n
+        }
+        if let _ = scanString("("),
+           let n = scanExpr(),
+           let _ = scanString(")") {
+            return n
+        }
+        return nil
+    }
+
+}
+
+public func evalExpr(_ input: String) -> Int {
+    let scanner = Scanner(string: input)
+    return scanner.scanExpr()!
+}
+
+fileprivate let input = Bundle.module.text(named: "day18")
+
 public func day18_1() -> Int {
-    0
+    input.lines().map(evalExpr).sum()
 }
 
 public func day18_2() -> Int {
--- Tests/AoC2020Tests/day18.swift
+++ Tests/AoC2020Tests/day18.swift
@@ -3,8 +3,16 @@ import AoC2020
 
 final class Day18Tests: XCTestCase {
 
+    func testPart1Eaxmple1() {
+        XCTAssertEqual(evalExpr("1 + 2 * 3 + 4 * 5 + 6"), 71)
+    }
+
+    func testPart1Eaxmple2() {
+        XCTAssertEqual(evalExpr("1 + (2 * 3) + (4 * (5 + 6))"), 51)
+    }
+
     func testPart1() {
-        XCTAssertEqual(day18_1(), 0)
+        XCTAssertEqual(day18_1(), 45283905029161)
     }
 
     func testPart2() {

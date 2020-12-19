a01d8d79556c5003f588267d309a8870f460d23d
Ben Lings <ben.lings@gmail.com>
2020-12-19 11:15:56 +0000

    Parse Day 19 rules

--- Sources/AoC2020/day19.swift
+++ Sources/AoC2020/day19.swift
@@ -1,6 +1,79 @@
 import Foundation
 import AdventCore
 
+public indirect enum MessageRule : Equatable {
+    case character(Character)
+    case consecutive([Int])
+    case alternative(MessageRule, MessageRule)
+}
+
+public struct MessageRules {
+    public var rules: [Int : MessageRule]
+}
+
+public extension MessageRules {
+    init(_ description: String) {
+        rules = description.lines().map { line in
+            let s = Scanner(string: line)
+            let num = s.scanInt()!
+            let _ = s.scanString(":")
+            let rule = s.scanRule()!
+            return (num, rule)
+        }.toDictionary()
+    }
+}
+
+// rule = consecutive | alternative | character
+// consecutive = { int }
+// alternative = consecutive "|" consecutive
+// character = '"' c '"'
+
+fileprivate extension Scanner {
+
+    func scanRule() -> MessageRule? {
+        if let a = scanAlternative() {
+            return a
+        } else if let c = scanConsecutive() {
+            return c
+        } else if let c = scanQuotedCharacter() {
+            return c
+        } else {
+            return nil
+        }
+    }
+
+    func scanQuotedCharacter() -> MessageRule? {
+        let i = currentIndex
+        guard let _ = scanString("\""),
+              let c = scanCharacter(),
+              let _ = scanCharacter() else {
+            currentIndex = i
+            return nil
+        }
+        return .character(c)
+    }
+
+    func scanConsecutive() -> MessageRule? {
+        guard let first = scanInt() else { return nil }
+        var r = [first]
+        while let next = scanInt() {
+            r.append(next)
+        }
+        return .consecutive(r)
+    }
+
+    func scanAlternative() -> MessageRule? {
+        let i = currentIndex
+        guard let first = scanConsecutive(),
+              let _ = scanString("|"),
+              let second = scanConsecutive() else {
+            currentIndex = i
+            return nil
+        }
+        return .alternative(first, second)
+    }
+}
+
 public func day19_1() -> Int {
     0
 }
--- Tests/AoC2020Tests/day19.swift
+++ Tests/AoC2020Tests/day19.swift
@@ -3,6 +3,22 @@ import AoC2020
 
 final class Day19Tests: XCTestCase {
 
+    func testPart1Example1() {
+        let input = """
+        0: 1 2
+        1: "a"
+        2: 1 3 | 3 1
+        3: "b"
+        """
+        let rules = MessageRules(input)
+        XCTAssertEqual(rules.rules, [
+            0: .consecutive([1, 2]),
+            1: .character("a"),
+            2: .alternative(.consecutive([1, 3]), .consecutive([3, 1])),
+            3: .character("b"),
+        ])
+    }
+
     func testPart1() {
         XCTAssertEqual(day19_1(), 0)
     }

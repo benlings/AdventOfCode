0be91b9d6b7f18699f6cf711f08c3ffb8eb10ff6
Ben Lings <ben.lings@gmail.com>
2020-12-25 21:19:29 +0000

    Move Offset to AdventCore

--- Sources/AdventCore/Offset.swift
+++ Sources/AdventCore/Offset.swift
@@ -1,10 +1,15 @@
 import Foundation
 
-struct Offset {
+public struct Offset {
     var east: Int = 0
     var north: Int = 0
 
-    mutating func rotate(angle: Int) {
+    public init(east: Int = 0, north: Int = 0) {
+        self.east = east
+        self.north = north
+    }
+
+    public mutating func rotate(angle: Int) {
         let newEast =
             east * Int(cos(Double.pi * Double(angle)/180.0)) -
             north * Int(sin(Double.pi * Double(angle)/180.0))
@@ -14,28 +19,28 @@ struct Offset {
         self = Offset(east: newEast, north: newNorth)
     }
 
-    func manhattanDistance(to other: Offset) -> Int {
+    public func manhattanDistance(to other: Offset) -> Int {
         let difference = other - self
         return abs(difference.east) + abs(difference.north)
     }
 }
 
 extension Offset : AdditiveArithmetic {
-    static func - (lhs: Offset, rhs: Offset) -> Offset {
+    public static func - (lhs: Offset, rhs: Offset) -> Offset {
         Offset(east: lhs.east - rhs.east, north: lhs.north - rhs.north)
     }
 
-    static func + (lhs: Offset, rhs: Offset) -> Offset {
+    public static func + (lhs: Offset, rhs: Offset) -> Offset {
         Offset(east: lhs.east + rhs.east, north: lhs.north + rhs.north)
     }
 
-    static var zero: Offset {
+    public static var zero: Offset {
         Offset()
     }
 }
 
 extension Offset {
-    static func * (lhs: Int, rhs: Offset) -> Offset {
+    public static func * (lhs: Int, rhs: Offset) -> Offset {
         Offset(east: lhs * rhs.east, north: lhs * rhs.north)
     }
 }

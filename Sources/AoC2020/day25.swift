db37fc295737bda14706cf343f12051031a32f4d
Ben Lings <ben.lings@gmail.com>
2020-12-25 07:42:16 +0000

    Day 25 part 1

--- Sources/AoC2020/day25.swift
+++ Sources/AoC2020/day25.swift
@@ -1,8 +1,51 @@
 import Foundation
 import AdventCore
 
+public struct HotelCypher {
+
+    let subject = 7
+    let modulus = 20201227
+
+    var cardPublicKey: Int
+    var doorPublicKey: Int
+
+    func findLoopSize(_ publicKey: Int) -> Int {
+        var value = 1
+        var loopCount = 0
+        while value != publicKey {
+            value = value * subject % modulus
+            loopCount += 1
+        }
+        return loopCount
+    }
+
+    func transform(_ subjectNumber: Int, loopSize: Int) -> Int {
+        var value = 1
+        for _ in 0..<loopSize {
+            value = value * subjectNumber % modulus
+        }
+        return value
+    }
+
+    public func findEncryptionKey() -> Int {
+        let cardLoopSize = findLoopSize(cardPublicKey)
+        return transform(doorPublicKey, loopSize: cardLoopSize)
+    }
+
+}
+
+public extension HotelCypher {
+    init(_ description: String) {
+        let keys: [Int] = description.lines().ints()
+        cardPublicKey = keys[0]
+        doorPublicKey = keys[1]
+    }
+}
+
+fileprivate let input = Bundle.module.text(named: "day25")
+
 public func day25_1() -> Int {
-    0
+    HotelCypher(input).findEncryptionKey()
 }
 
 public func day25_2() -> Int {
--- Tests/AoC2020Tests/day25.swift
+++ Tests/AoC2020Tests/day25.swift
@@ -3,8 +3,16 @@ import AoC2020
 
 final class Day25Tests: XCTestCase {
 
+    func testPart1Example() {
+        let input = """
+        5764801
+        17807724
+        """
+        XCTAssertEqual(HotelCypher(input).findEncryptionKey(), 14897079)
+    }
+
     func testPart1() {
-        XCTAssertEqual(day25_1(), 0)
+        XCTAssertEqual(day25_1(), 10548634)
     }
 
     func testPart2() {

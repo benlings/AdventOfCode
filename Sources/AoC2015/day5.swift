b42397c53aef59b4b4cf07766d27737dadc17d12
Ben Lings <ben.lings@gmail.com>
2021-01-03 19:23:05 +0000

    2015: Day 5

--- Sources/AoC2015/day5.swift
+++ Sources/AoC2015/day5.swift
@@ -1,10 +1,40 @@
 import Foundation
 import AdventCore
 
+/**
+ A nice string is one with all of the following properties:
+
+ * It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+ * It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+ * It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+ */
+
+public func isNicePart1(_ string: String) -> Bool {
+    let vowels = CharacterSet(charactersIn: "aeiou")
+    let contains3Vowels = string.unicodeScalars.count(where: vowels.contains) >= 3
+    let containsRepeatedLetter = string.range(of: "([a-z])(\\1)", options: .regularExpression) != nil
+    let containsMagicString = string.range(of: "(ab|cd|pq|xy)", options: .regularExpression) != nil
+    return contains3Vowels && containsRepeatedLetter && !containsMagicString
+}
+
+/**
+ a nice string is one with all of the following properties:
+
+ * It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+ * It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+ */
+public func isNicePart2(_ string: String) -> Bool {
+    let containsPairTwice = string.range(of: "([a-z]{2}).*(\\1)", options: .regularExpression) != nil
+    let containsRepeatedLetterWithLetterBetween = string.range(of: "([a-z])[a-z](\\1)", options: .regularExpression) != nil
+    return containsPairTwice && containsRepeatedLetterWithLetterBetween
+}
+
+private let input = Bundle.module.text(named: "day5")
+
 public func day5_1() -> Int {
-    0
+    input.lines().count(where: isNicePart1)
 }
 
 public func day5_2() -> Int {
-    0
+    input.lines().count(where: isNicePart2)
 }
--- Tests/AoC2015Tests/day5.swift
+++ Tests/AoC2015Tests/day5.swift
@@ -3,11 +3,26 @@ import AoC2015
 
 final class Day5Tests: XCTestCase {
 
+    func testPart1Examples() {
+        XCTAssertEqual(isNicePart1("ugknbfddgicrmopn"), true)
+        XCTAssertEqual(isNicePart1("aaa"), true)
+        XCTAssertEqual(isNicePart1("jchzalrnumimnmhp"), false)
+        XCTAssertEqual(isNicePart1("haegwjzuvuyypxyu"), false)
+        XCTAssertEqual(isNicePart1("dvszwmarrgswjxmb"), false)
+    }
+
     func testPart1() {
-        XCTAssertEqual(day5_1(), 0)
+        XCTAssertEqual(day5_1(), 255)
+    }
+
+    func testPart2Examples() {
+        XCTAssertEqual(isNicePart2("qjhvhtzxzqqjkmpb"), true)
+        XCTAssertEqual(isNicePart2("xxyxx"), true)
+        XCTAssertEqual(isNicePart2("uurcxstgmygtbstg"), false)
+        XCTAssertEqual(isNicePart2("ieodomkazucvgmuy"), false)
     }
 
     func testPart2() {
-        XCTAssertEqual(day5_2(), 0)
+        XCTAssertEqual(day5_2(), 55)
     }
 }

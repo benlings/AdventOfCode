5f5445d9f0cd5f7e2f3fd5998cbae77c6131d653
Ben Lings <ben.lings@gmail.com>
2021-01-14 21:55:43 +0000

    2015: Day 7 part 1 example

--- Sources/AoC2015/day7.swift
+++ Sources/AoC2015/day7.swift
@@ -1,6 +1,91 @@
 import Foundation
 import AdventCore
 
+public struct Circuit {
+    public typealias WireId = String
+    public typealias Signal = UInt16
+
+    enum WireInput {
+        case signal(Signal)
+        case and(WireId, WireId)
+        case or(WireId, WireId)
+        case lshift(WireId, Int)
+        case rshift(WireId, Int)
+        case not(WireId)
+    }
+
+    typealias WireConfiguration = [WireId : WireInput]
+
+    var configuration: WireConfiguration
+
+    public func signal(id: WireId) -> Signal {
+        switch configuration[id] {
+        case .signal(let s): return s
+        case .and(let lhs, let rhs): return signal(id: lhs) & signal(id: rhs)
+        case .or(let lhs, let rhs): return signal(id: lhs) | signal(id: rhs)
+        case .lshift(let lhs, let rhs): return signal(id: lhs) << rhs
+        case .rshift(let lhs, let rhs): return signal(id: lhs) >> rhs
+        case .not(let arg): return ~signal(id: arg)
+        default:
+            preconditionFailure()
+        }
+    }
+}
+
+extension Scanner {
+    func scanWireConfig() -> (Circuit.WireId, Circuit.WireInput)? {
+        guard let input = scanWireInput(),
+              let _ = scanString("->"),
+              let id = scanWireId() else {
+            return nil
+        }
+        return (id, input)
+    }
+
+    func scanWireId() -> Circuit.WireId? {
+        scanUpToCharacters(from: .whitespacesAndNewlines)
+    }
+
+    func scanWireInput() -> Circuit.WireInput? {
+        if let i = scanInt(),
+           let s = UInt16(exactly: i) {
+            return .signal(s)
+        }
+        if scanString("NOT") != nil,
+           let id = scanWireId() {
+            return .not(id)
+        }
+        if let lhs = scanWireId() {
+            switch scanUpToCharacters(from: .whitespaces) {
+            case "AND":
+                return scanWireId().map { .and(lhs, $0) }
+            case "OR":
+                return scanWireId().map { .or(lhs, $0) }
+            case "LSHIFT":
+                return scanInt().map { .lshift(lhs, $0) }
+            case "RSHIFT":
+                return scanInt().map { .rshift(lhs, $0) }
+            default:
+                return nil
+            }
+        }
+        return nil
+    }
+
+
+}
+
+public extension Circuit {
+    init(_ description: String) {
+        configuration = description
+            .lines()
+            .compactMap {
+                Scanner(string: $0).scanWireConfig()
+            }
+            .toDictionary()
+    }
+}
+
 public func day7_1() -> Int {
     0
 }
--- Tests/AoC2015Tests/day7.swift
+++ Tests/AoC2015Tests/day7.swift
@@ -3,6 +3,28 @@ import AoC2015
 
 final class Day7Tests: XCTestCase {
 
+    func testPart1Example() {
+        let input = """
+        123 -> x
+        456 -> y
+        x AND y -> d
+        x OR y -> e
+        x LSHIFT 2 -> f
+        y RSHIFT 2 -> g
+        NOT x -> h
+        NOT y -> i
+        """
+        let circuit = Circuit(input)
+        XCTAssertEqual(circuit.signal(id: "d"), 72)
+        XCTAssertEqual(circuit.signal(id: "e"), 507)
+        XCTAssertEqual(circuit.signal(id: "f"), 492)
+        XCTAssertEqual(circuit.signal(id: "g"), 114)
+        XCTAssertEqual(circuit.signal(id: "h"), 65412)
+        XCTAssertEqual(circuit.signal(id: "i"), 65079)
+        XCTAssertEqual(circuit.signal(id: "x"), 123)
+        XCTAssertEqual(circuit.signal(id: "y"), 456)
+    }
+
     func testPart1() {
         XCTAssertEqual(day7_1(), 0)
     }

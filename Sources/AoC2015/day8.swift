4801da3f52c1eef07c3dc1d8fe921347eaefabdb
Ben Lings <ben.lings@gmail.com>
2021-02-16 20:44:35 +0000

    2015: Scaffold day 8

---
+++ Sources/AoC2015/day8.swift
@@ -0,0 +1,10 @@
+import Foundation
+import AdventCore
+
+public func day8_1() -> Int {
+    0
+}
+
+public func day8_2() -> Int {
+    0
+}
---
+++ Tests/AoC2015Tests/day8.swift
@@ -0,0 +1,13 @@
+import XCTest
+import AoC2015
+
+final class Day8Tests: XCTestCase {
+
+    func testPart1() {
+        XCTAssertEqual(day8_1(), 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day8_2(), 0)
+    }
+}

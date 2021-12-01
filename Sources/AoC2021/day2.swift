2725f56ca06641d1662e5f21935da5a89c623f9e
Ben Lings <ben.lings@gmail.com>
2021-12-01 08:25:43 +0000

    Scaffold day 2

---
+++ Sources/AoC2021/day2.swift
@@ -0,0 +1,12 @@
+import Foundation
+import AdventCore
+
+fileprivate let day2_input = Bundle.module.text(named: "day2").lines()
+
+public func day2_1() -> Int {
+    0
+}
+
+public func day2_2() -> Int {
+    0
+}

7a1b10f8b1aefd2f977dd4cc48c417db1f02b6f4
Ben Lings <ben.lings@gmail.com>
2021-12-03 07:40:31 +0000

    Day 3 part 1

--- Sources/AoC2021/day3.swift
+++ Sources/AoC2021/day3.swift
@@ -1,10 +1,46 @@
 import Foundation
 import AdventCore
 
-fileprivate let day3_input = Bundle.module.text(named: "day3").lines()
+public func toInt(binary: [Int]) -> Int {
+    binary.reduce(0) { $0 * 2 + $1 }
+}
+
+public struct SubmarineDiagnostic {
+    var numbers: [[Int]]
+
+    func totals() -> [Int] {
+        let indices = numbers.first!.indices
+        return indices.map { i in
+            numbers.map { $0[i] }.sum()
+        }
+    }
+
+    public func gammaRate() -> [Int] {
+        let count = numbers.count
+        return totals().map { $0 > count / 2 ? 1 : 0 }
+    }
+
+    public func epsilonRate() -> [Int] {
+        let count = numbers.count
+        return totals().map { $0 <= count / 2 ? 1 : 0 }
+    }
+
+    public func powerConsumption() -> Int {
+        toInt(binary: gammaRate()) * toInt(binary: epsilonRate())
+    }
+}
+
+public extension SubmarineDiagnostic {
+    init(_ report: String) {
+        self.numbers = report.lines().map { $0.toArray().compactMap { $0.wholeNumberValue } }
+    }
+}
+
+
+fileprivate let day3_input = Bundle.module.text(named: "day3")
 
 public func day3_1() -> Int {
-    0
+    SubmarineDiagnostic(day3_input).powerConsumption()
 }
 
 public func day3_2() -> Int {
--- Tests/AoC2021Tests/day3.swift
+++ Tests/AoC2021Tests/day3.swift
@@ -3,8 +3,37 @@ import AoC2021
 
 final class Day3Tests: XCTestCase {
 
+    let exampleInput = """
+    00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010
+    """
+
+    func testBinaryToInt()
+    {
+        XCTAssertEqual(toInt(binary: [1,0,1,1,0]), 22)
+        XCTAssertEqual(toInt(binary: [0,1,0,0,1]), 9)
+    }
+
+    func testPart1Example()
+    {
+        let report = SubmarineDiagnostic(exampleInput)
+        XCTAssertEqual(report.gammaRate(), [1,0,1,1,0])
+        XCTAssertEqual(report.epsilonRate(), [0,1,0,0,1])
+        XCTAssertEqual(report.powerConsumption(), 198)
+    }
+
     func testPart1() {
-        XCTAssertEqual(day3_1(), 0)
+        XCTAssertEqual(day3_1(), 3687446)
     }
 
     func testPart2() {

90bcf61b5566cd208d65c1c10fdd77431a4b1349
Ben Lings <ben.lings@gmail.com>
2021-12-06 07:25:04 +0000

    Day 6 part 1

--- Sources/AoC2021/day6.swift
+++ Sources/AoC2021/day6.swift
@@ -1,10 +1,48 @@
 import Foundation
 import AdventCore
 
-fileprivate let day6_input = Bundle.module.text(named: "day6").lines()
+struct LanternFish {
+    var timer: Int = 8
+
+    mutating func tick() -> LanternFish? {
+        if timer == 0 {
+            timer = 6
+            return LanternFish()
+        } else {
+            timer -= 1
+            return nil
+        }
+    }
+}
+
+public struct School {
+    var fish: [LanternFish]
+
+    mutating func tick() {
+        var spawned = [LanternFish]()
+        for i in fish.indices {
+            let new = fish[i].tick()
+            if let new = new {
+                spawned.append(new)
+            }
+        }
+        fish += spawned
+    }
+
+    public static func simulate(ages: [Int], days: Int) -> Int {
+        var school = School(fish: ages.map(LanternFish.init))
+        for _ in 0..<days {
+            school.tick()
+        }
+        return school.fish.count
+    }
+}
+
+
+fileprivate let day6_input: [Int] = Bundle.module.text(named: "day6").lines()[0].commaSeparated().ints()
 
 public func day6_1() -> Int {
-    0
+    School.simulate(ages: day6_input, days: 80)
 }
 
 public func day6_2() -> Int {
--- Tests/AoC2021Tests/day6.swift
+++ Tests/AoC2021Tests/day6.swift
@@ -3,8 +3,15 @@ import AoC2021
 
 final class Day6Tests: XCTestCase {
 
+    let exampleInput = "3,4,3,1,2"
+
+    func testPart1Example() {
+        let count = School.simulate(ages: exampleInput.commaSeparated().ints(), days: 80)
+        XCTAssertEqual(count, 5934)
+    }
+
     func testPart1() {
-        XCTAssertEqual(day6_1(), 0)
+        XCTAssertEqual(day6_1(), 353274)
     }
 
     func testPart2() {

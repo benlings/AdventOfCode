11cd02bb598d39e44a2ca04477f809be6be2b354
Ben Lings <ben.lings@gmail.com>
2021-12-07 07:26:14 +0000

    Day 7 part 1

--- Sources/AoC2021/day7.swift
+++ Sources/AoC2021/day7.swift
@@ -1,10 +1,30 @@
 import Foundation
 import AdventCore
 
-fileprivate let day7_input = Bundle.module.text(named: "day7").lines()
+public struct CrabSwarm {
+    var positions: [Int]
+
+    func fuelCost(_ pos: Int) -> Int {
+        positions.reduce(0) { $0 + abs($1 - pos) }
+    }
+
+    public func findMinFuelCost() -> Int {
+        let (min, max) = positions.minAndMax()!
+        return (min...max).map { fuelCost($0) }.min()!
+    }
+
+}
+
+public extension CrabSwarm {
+    init(_ description: String) {
+        self.positions = description.commaSeparated().ints()
+    }
+}
+
+fileprivate let day7_input = Bundle.module.text(named: "day7").lines()[0]
 
 public func day7_1() -> Int {
-    0
+    CrabSwarm(day7_input).findMinFuelCost()
 }
 
 public func day7_2() -> Int {
--- Tests/AoC2021Tests/day7.swift
+++ Tests/AoC2021Tests/day7.swift
@@ -5,8 +5,12 @@ final class Day7Tests: XCTestCase {
 
     let exampleInput = "16,1,2,0,4,2,7,1,2,14"
 
+    func testPart1Example() {
+        XCTAssertEqual(CrabSwarm(exampleInput).findMinFuelCost(), 37)
+    }
+
     func testPart1() {
-        XCTAssertEqual(day7_1(), 0)
+        XCTAssertEqual(day7_1(), 331067)
     }
 
     func testPart2() {

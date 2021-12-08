e9f26cbb38aee5aab639ffd660acd1a924c24d9a
Ben Lings <ben.lings@gmail.com>
2021-12-08 08:31:39 +0000

    Day 8 part 1

--- Sources/AoC2021/day8.swift
+++ Sources/AoC2021/day8.swift
@@ -1,10 +1,44 @@
 import Foundation
 import AdventCore
 
+enum Wire : Character {
+    case a = "a", b = "b", c = "c", d = "d", e = "e", f = "f", g = "g"
+}
+
+public struct DisplaySignals {
+    var signalPatterns: Set<[Wire]>
+    var output: [Set<Wire>]
+
+    public static func count1478(input: [String]) -> Int {
+        let output1478Counts: Set<Int> = [2, 4, 3, 7]
+        let signals = input.map(DisplaySignals.init)
+        return signals.map { signal in
+            signal.output.count { output1478Counts.contains($0.count) }
+        }.sum()
+    }
+}
+
+extension DisplaySignals {
+    init(_ description: String) {
+        let scanner = Scanner(string: description)
+        let possibleWires = CharacterSet(charactersIn: "abcdefg")
+        var patterns = Set<[Wire]>()
+        while let wires = scanner.scanCharacters(from: possibleWires) {
+            patterns.update(with: wires.compactMap(Wire.init(rawValue:)))
+        }
+        _ = scanner.scanString("|")
+        var output = [Set<Wire>]()
+        while let wires = scanner.scanCharacters(from: possibleWires) {
+            output.append(wires.compactMap(Wire.init(rawValue:)).toSet())
+        }
+        self = .init(signalPatterns: patterns, output: output)
+    }
+}
+
 fileprivate let day8_input = Bundle.module.text(named: "day8").lines()
 
 public func day8_1() -> Int {
-    0
+    DisplaySignals.count1478(input: day8_input)
 }
 
 public func day8_2() -> Int {
--- Tests/AoC2021Tests/day8.swift
+++ Tests/AoC2021Tests/day8.swift
@@ -3,8 +3,25 @@ import AoC2021
 
 final class Day8Tests: XCTestCase {
 
+    let exampleInput = """
+    be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+    fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+    fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+    aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+    fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+    dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+    bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+    egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+    gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    """.lines()
+
+    func testPart1Example() {
+        XCTAssertEqual(DisplaySignals.count1478(input: exampleInput), 26)
+    }
+
     func testPart1() {
-        XCTAssertEqual(day8_1(), 0)
+        XCTAssertEqual(day8_1(), 390)
     }
 
     func testPart2() {

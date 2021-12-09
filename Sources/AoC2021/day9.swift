99437b87049ba2720093e41023573b7e892a88c5
Ben Lings <ben.lings@gmail.com>
2021-12-09 07:03:31 +0000

    Scaffold day 9

---
+++ Sources/AoC2021/day9.swift
@@ -0,0 +1,12 @@
+import Foundation
+import AdventCore
+
+fileprivate let day9_input = Bundle.module.text(named: "day9").lines()
+
+public func day9_1() -> Int {
+    0
+}
+
+public func day9_2() -> Int {
+    0
+}
---
+++ Tests/AoC2021Tests/day9.swift
@@ -0,0 +1,13 @@
+import XCTest
+import AoC2021
+
+final class Day9Tests: XCTestCase {
+
+    func testPart1() {
+        XCTAssertEqual(day9_1(), 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day9_2(), 0)
+    }
+}

b2ffd1570f5b2d1b5e0189075c9e7e641c651de7
Ben Lings <ben.lings@gmail.com>
2021-12-13 07:51:01 +0000

    Day 13 part 1

--- Sources/AoC2021/day13.swift
+++ Sources/AoC2021/day13.swift
@@ -1,10 +1,82 @@
 import Foundation
 import AdventCore
 
-fileprivate let day13_input = Bundle.module.text(named: "day13").lines()
+public struct TransparentPaper {
+
+    enum FoldInstruction {
+        case up(y: Int)
+        case left(x: Int)
+    }
+
+    var dots: Set<Offset>
+    var foldInstructions: [FoldInstruction]
+
+    mutating func fold(instruction: FoldInstruction) {
+        var newDots = dots
+        for dot in dots {
+            guard let newDot = instruction.transform(dot: dot) else { continue }
+            newDots.remove(dot)
+            newDots.insert(newDot)
+        }
+        dots = newDots
+    }
+
+}
+
+extension TransparentPaper.FoldInstruction {
+    func transform(dot: Offset) -> Offset? {
+        switch self {
+        case .up(let y):
+            return dot.north > y ? Offset(east: dot.east, north: 2 * y - dot.north) : nil
+        case .left(let x):
+            return dot.east > x ? Offset(east: 2 * x - dot.east, north: dot.north) : nil
+        }
+    }
+}
+
+
+extension Scanner {
+    func scanFoldInstruction() -> TransparentPaper.FoldInstruction? {
+        if let _ = scanString("fold along"),
+           let axis = scanCharacters(from: .init(charactersIn: "xy")),
+           let _ = scanString("="),
+           let value = scanInt() {
+            switch axis {
+            case "x": return .left(x: value)
+            case "y": return .up(y: value)
+            default: fatalError()
+            }
+        } else {
+            return nil
+        }
+    }
+}
+
+extension TransparentPaper {
+    init(_ description: String) {
+        let groups = description.groups()
+        self.dots = groups[0].lines().map { line -> Offset in
+            let components = line.commaSeparated().ints() as [Int]
+            return Offset(east: components[0], north: components[1])
+        }.toSet()
+        self.foldInstructions = groups[1].lines().compactMap { line in
+            Scanner(string: line).scanFoldInstruction()
+        }
+    }
+
+    public static func visibleDotsAfterFirstFold(_ description: String) -> Int {
+        var paper = Self(description)
+        paper.fold(instruction: paper.foldInstructions[0])
+        return paper.dots.count
+    }
+
+}
+
+
+fileprivate let day13_input = Bundle.module.text(named: "day13")
 
 public func day13_1() -> Int {
-    0
+    TransparentPaper.visibleDotsAfterFirstFold(day13_input)
 }
 
 public func day13_2() -> Int {
--- Tests/AoC2021Tests/day13.swift
+++ Tests/AoC2021Tests/day13.swift
@@ -4,14 +4,35 @@ import AoC2021
 final class Day13Tests: XCTestCase {
 
     let exampleInput = """
+    6,10
+    0,14
+    9,10
+    0,3
+    10,4
+    4,11
+    6,0
+    6,12
+    4,1
+    0,13
+    10,12
+    3,4
+    3,0
+    8,4
+    1,10
+    2,14
+    8,10
+    9,0
+
+    fold along y=7
+    fold along x=5
     """
 
     func testPart1Example() {
-        XCTAssertEqual(0, 0)
+        XCTAssertEqual(TransparentPaper.visibleDotsAfterFirstFold(exampleInput), 17)
     }
 
     func testPart1() {
-        XCTAssertEqual(day13_1(), 0)
+        XCTAssertEqual(day13_1(), 687)
     }
 
     func testPart2Example() {

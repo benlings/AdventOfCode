539bed54503614df64ed9bb7e11c04d24e1b58ac
Ben Lings <ben.lings@gmail.com>
2021-12-14 07:05:36 +0000

    Scaffold day 14

---
+++ Sources/AoC2021/day14.swift
@@ -0,0 +1,12 @@
+import Foundation
+import AdventCore
+
+fileprivate let day14_input = Bundle.module.text(named: "day14").lines()
+
+public func day14_1() -> Int {
+    0
+}
+
+public func day14_2() -> Int {
+    0
+}
---
+++ Tests/AoC2021Tests/day14.swift
@@ -0,0 +1,42 @@
+import XCTest
+import AoC2021
+
+final class Day14Tests: XCTestCase {
+
+    let exampleInput = """
+    NNCB
+
+    CH -> B
+    HH -> N
+    CB -> H
+    NH -> C
+    HB -> C
+    HC -> B
+    HN -> C
+    NN -> C
+    BH -> H
+    NC -> B
+    NB -> B
+    BN -> B
+    BB -> N
+    BC -> B
+    CC -> N
+    CN -> C
+    """
+
+    func testPart1Example() {
+        XCTAssertEqual(0, 0)
+    }
+
+    func testPart1() {
+        XCTAssertEqual(day14_1(), 0)
+    }
+
+    func testPart2Example() {
+        XCTAssertEqual(0, 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day14_2(), 0)
+    }
+}

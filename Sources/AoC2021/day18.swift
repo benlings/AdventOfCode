a938c49fbe1b2101594e12985304f22ab793837f
Ben Lings <ben.lings@gmail.com>
2021-12-18 08:30:37 +0000

    Parse numbers

--- Sources/AoC2021/day18.swift
+++ Sources/AoC2021/day18.swift
@@ -1,6 +1,41 @@
 import Foundation
 import AdventCore
 
+public indirect enum SnailfishNumber : Equatable {
+    case pair(SnailfishNumber, SnailfishNumber)
+    case regular(Int)
+}
+
+fileprivate extension Scanner {
+    func scanSnailfishNumber() -> SnailfishNumber? {
+        let i = currentIndex
+        if let _ = scanString("["),
+           let first = scanIntOrSnailFish(),
+           let _ = scanString(","),
+           let second = scanIntOrSnailFish(),
+           let _ = scanString("]") {
+            return .pair(first, second)
+        } else {
+            currentIndex = i
+            return nil
+        }
+    }
+    func scanIntOrSnailFish() -> SnailfishNumber? {
+        if let number = scanInt() {
+            return .regular(number)
+        } else {
+            return scanSnailfishNumber()
+        }
+    }
+}
+
+public extension SnailfishNumber {
+    init(_ description: String) {
+        let scanner = Scanner(string: description)
+        self = scanner.scanSnailfishNumber()!
+    }
+}
+
 fileprivate let day18_input = Bundle.module.text(named: "day18").lines()
 
 public func day18_1() -> Int {
--- Tests/AoC2021Tests/day18.swift
+++ Tests/AoC2021Tests/day18.swift
@@ -3,11 +3,11 @@ import AoC2021
 
 final class Day18Tests: XCTestCase {
 
-    let exampleInput = """
-    """
+    let exampleInput = "[[1,9],[8,5]]"
 
-    func testPart1Example() {
-        XCTAssertEqual(0, 0)
+    func testParseNumber() {
+        let n = SnailfishNumber(exampleInput)
+        XCTAssertEqual(n, .pair(.pair(.regular(1), .regular(9)), .pair(.regular(8), .regular(5))))
     }
 
     func testPart1() {

e2ec1e2e8390d6b3fe361d6e5006b13a202d7bc4
Ben Lings <ben.lings@gmail.com>
2021-12-18 08:13:29 +0000

    Scaffold Day 18

---
+++ Sources/AoC2021/day18.swift
@@ -0,0 +1,12 @@
+import Foundation
+import AdventCore
+
+fileprivate let day18_input = Bundle.module.text(named: "day18").lines()
+
+public func day18_1() -> Int {
+    0
+}
+
+public func day18_2() -> Int {
+    0
+}

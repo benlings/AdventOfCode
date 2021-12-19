65f3bd245e90dea4310ace7f90cf8e5f0a5a70a0
Ben Lings <ben.lings@gmail.com>
2021-12-19 19:11:48 +0000

    Implement orientations

--- Sources/AoC2021/day19.swift
+++ Sources/AoC2021/day19.swift
@@ -1,6 +1,103 @@
 import Foundation
 import AdventCore
 
+public struct Offset3D : Hashable {
+
+    var x, y, z: Int
+
+    public init(x: Int = 0, y: Int = 0, z: Int = 0) {
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+
+}
+
+public enum Axis3D {
+    case x, y, z
+}
+
+public extension Offset3D {
+
+    func rotated(angle: Int, axis: Axis3D) -> Offset3D {
+        let radians = Double.pi * Double(angle)/180.0
+        let s = Int(sin(radians))
+        let c = Int(cos(radians))
+        switch axis {
+        case .x:
+            return Offset3D(x: x,
+                            y: y * c - z * s,
+                            z: y * s + z * c)
+        case .y:
+            return Offset3D(x: z * s + x * c,
+                            y: y,
+                            z: z * c - x * s)
+        case .z:
+            return Offset3D(x: x * c - y * s,
+                            y: x * s + y * c,
+                            z: z)
+        }
+    }
+
+    func allOrientations(axis: Axis3D) -> [Offset3D] {
+        return [
+            rotated(angle: 0, axis: axis),
+            rotated(angle: 90, axis: axis),
+            rotated(angle: 180, axis: axis),
+            rotated(angle: 270, axis: axis),
+        ]
+    }
+
+    func orientations() -> [Offset3D] {
+        return [
+            // Orientations around x
+            (self, .x), (self.rotated(angle: 180, axis: .y), .x),
+            // First move x axis to the y axis, then orientations around y
+            (self.rotated(angle: 90, axis: .z), .y), (self.rotated(angle: -90, axis: .z), .y),
+            // First move x axis to the z axis, then orientations around z
+            (self.rotated(angle: 90, axis: .y), .z), (self.rotated(angle: -90, axis: .y), .z),
+        ].flatMap { $0.allOrientations(axis: $1) }
+    }
+
+}
+
+public struct ScannerReadings {
+    var scannerResults: [Int: Set<Offset3D>]
+}
+
+fileprivate extension Scanner {
+
+    func scanOffset3D() -> Offset3D? {
+        if let x = scanInt(),
+           let _ = scanString(","),
+           let y = scanInt(),
+           let _ = scanString(","),
+           let z = scanInt() {
+            return Offset3D(x: x, y: y, z: z)
+        } else { return nil }
+    }
+
+    func scanScanner() -> (Int, Set<Offset3D>)? {
+        guard let _ = scanString("--- scanner"),
+              let id = scanInt(),
+              let _ = scanString("---")
+        else { return nil }
+        var beacons = Set<Offset3D>()
+        while let beacon = scanOffset3D() {
+            beacons.insert(beacon)
+        }
+        return (id, beacons)
+    }
+}
+
+public extension ScannerReadings {
+    init(_ description: String) {
+        self.scannerResults = description.groups().compactMap { group in
+            Scanner(string: group).scanScanner()
+        }.toDictionary()
+    }
+}
+
 fileprivate let day19_input = Bundle.module.text(named: "day19").lines()
 
 public func day19_1() -> Int {
--- Tests/AoC2021Tests/day19.swift
+++ Tests/AoC2021Tests/day19.swift
@@ -3,6 +3,15 @@ import AoC2021
 
 final class Day19Tests: XCTestCase {
 
+    func testOrientations() {
+        let axes = [Offset3D(x: 1), Offset3D(y: 1), Offset3D(z: 1)]
+        let orientations = axes.map { $0.orientations() }
+        let orientedAxes = zip(orientations[0], zip(orientations[1], orientations[2]))
+            .map { [$0.0, $0.1.0, $0.1.1] }
+        XCTAssertEqual(orientedAxes.count, 24)
+        XCTAssertEqual(orientedAxes.toSet().count, 24)
+    }
+
     let exampleInput = Bundle.module.text(named: "day19example")
 
     func testPart1Example() {

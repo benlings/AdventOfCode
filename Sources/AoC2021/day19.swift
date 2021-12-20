ceb30821d987f95de340b4e7965f928e64d90f01
Ben Lings <ben.lings@gmail.com>
2021-12-20 21:34:30 +0000

    Day 19 part 1 example

--- Sources/AoC2021/day19.swift
+++ Sources/AoC2021/day19.swift
@@ -13,6 +13,21 @@ public struct Offset3D : Hashable {
 
 }
 
+extension Offset3D : AdditiveArithmetic {
+    public static func - (lhs: Offset3D, rhs: Offset3D) -> Offset3D {
+        Offset3D(x: lhs.x - rhs.x, y: lhs.y - rhs.y, z: lhs.z - rhs.z)
+    }
+
+    public static func + (lhs: Offset3D, rhs: Offset3D) -> Offset3D {
+        Offset3D(x: lhs.x + rhs.x, y: lhs.y + rhs.y, z: lhs.z + rhs.z)
+    }
+
+    public static var zero: Offset3D {
+        Offset3D()
+    }
+}
+
+
 public enum Axis3D {
     case x, y, z
 }
@@ -72,15 +87,6 @@ public extension Offset3D {
 }
 
 public extension Set where Element == Offset3D {
-    func orientations() -> [Set<Offset3D>] {
-        var result = [Set<Offset3D>]()
-        let o = map { $0.orientations() }
-        for i in o[0].indices {
-            result.append(o.map { $0[i] }.toSet())
-        }
-        return result
-    }
-
     func projection(axis: Axis3D) -> (Int, IndexSet) {
         let p = map { $0[axis: axis] }
         // Ensure IndexSet starts from 0
@@ -100,11 +106,30 @@ public extension Array where Element == Offset3D {
     }
 }
 
-public struct ScannerReadings {
-    var scannerResults: [Int: Set<Offset3D>]
-    var positions: [Int: Offset3D] = [:]
+struct ScannerReport {
+    var beacons: Set<Offset3D>
+    var position = Offset3D()
 
-    var scanner0: Set<Offset3D> { scannerResults[0]! }
+    func orientations() -> [Set<Offset3D>] {
+        var result = [Set<Offset3D>]()
+        let o = beacons.map { $0.orientations() }
+        for i in o[0].indices {
+            result.append(o.map { $0[i] }.toSet())
+        }
+        return result
+    }
+
+    mutating func orient(_ i: Int) {
+        beacons = orientations()[i]
+    }
+
+}
+
+public struct ScannerReadings {
+    var scannerResults: [Int: ScannerReport]
+
+    var scanner0: ScannerReport { scannerResults[0]! }
+    var oriented = [0] as Set
 
     func findMatches(_ first: Set<Offset3D>, _ second: Set<Offset3D>, axis: Axis3D) -> [Int] {
         let (offset0, proj0) = first.projection(axis: axis)
@@ -123,28 +148,44 @@ public struct ScannerReadings {
         return result
     }
 
-    func findPosition(_ i: Int, axis: Axis3D) -> Int? {
+    func findPosition(from: ScannerReport, to i: Int, axis: Axis3D) -> (orientations: Set<Int>, offset: Int)? {
         let results = scannerResults[i]!.orientations().enumerated().map {
-            ($0.offset, findMatches(scanner0, $0.element, axis: axis))
-        }.filter { $0.1.count > 0 }
+            (orientation: $0.offset, matches: findMatches(from.beacons, $0.element, axis: axis))
+        }.filter { $0.matches.count > 0 }
         if results.count > 0 {
-            return results[0].1[0]
+            let offsets = results.flatMap(\.matches).toSet()
+            assert(offsets.count == 1)
+            return (orientations: results.map(\.orientation).toSet(), offset: offsets.first!)
         }
         return nil
     }
 
     public mutating func findMatches() {
-        for i in scannerResults.keys.filter({ $0 != 0 }) {
-            if let x = findPosition(i, axis: .x),
-               let y = findPosition(i, axis: .y),
-               let z = findPosition(i, axis: .z) {
-                positions[i] = Offset3D(x: x, y: y, z: z)
+        var searched = Set<Int>()
+        var toSearch = [0] as Set
+        while let j = toSearch.popFirst() {
+            let fromScanner = scannerResults[j]!
+            for i in scannerResults.keys.filter({ $0 != j && !oriented.contains($0) }) {
+                if let x = findPosition(from: fromScanner, to: i, axis: .x),
+                   let y = findPosition(from: fromScanner, to: i, axis: .y),
+                   let z = findPosition(from: fromScanner, to: i, axis: .z) {
+                    let orientations = x.orientations.intersection(y.orientations).intersection(z.orientations)
+                    assert(orientations.count == 1)
+                    let orientation = orientations.first!
+                    self.scannerResults[i]?.position = fromScanner.position + Offset3D(x: x.offset, y: y.offset, z: z.offset)
+                    self.scannerResults[i]?.orient(orientation)
+                    self.oriented.insert(i)
+                }
             }
+            searched.insert(j)
+            toSearch.formUnion(oriented.subtracting(searched))
         }
     }
 
     public func countUniqueBeacons() -> Int {
-        0
+        scannerResults.values.flatMap { report in
+            report.beacons.map { $0 + report.position }
+        }.toSet().count
     }
 }
 
@@ -160,7 +201,7 @@ fileprivate extension Scanner {
         } else { return nil }
     }
 
-    func scanScanner() -> (Int, Set<Offset3D>)? {
+    func scanScannerReport() -> (Int, ScannerReport)? {
         guard let _ = scanString("--- scanner"),
               let id = scanInt(),
               let _ = scanString("---")
@@ -169,14 +210,14 @@ fileprivate extension Scanner {
         while let beacon = scanOffset3D() {
             beacons.insert(beacon)
         }
-        return (id, beacons)
+        return (id, ScannerReport(beacons: beacons))
     }
 }
 
 public extension ScannerReadings {
     init(_ description: String) {
         self.scannerResults = description.groups().compactMap { group in
-            Scanner(string: group).scanScanner()
+            Scanner(string: group).scanScannerReport()
         }.toDictionary()
     }
 }

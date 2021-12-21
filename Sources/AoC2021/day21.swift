a0ae44f74d56323607f804ff83ee0fb06ceed744
Ben Lings <ben.lings@gmail.com>
2021-12-21 07:51:36 +0000

    Scaffold day 21

---
+++ Sources/AoC2021/day21.swift
@@ -0,0 +1,12 @@
+import Foundation
+import AdventCore
+
+fileprivate let day21_input = Bundle.module.text(named: "day21").lines()
+
+public func day21_1() -> Int {
+    0
+}
+
+public func day21_2() -> Int {
+    0
+}
---
+++ Tests/AoC2021Tests/day21.swift
@@ -0,0 +1,24 @@
+import XCTest
+import AoC2021
+
+final class Day21Tests: XCTestCase {
+
+    let exampleInput = """
+    """
+
+    func testPart1Example() {
+        XCTAssertEqual(0, 0)
+    }
+
+    func testPart1() {
+        XCTAssertEqual(day21_1(), 0)
+    }
+
+    func testPart2Example() {
+        XCTAssertEqual(0, 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day21_2(), 0)
+    }
+}

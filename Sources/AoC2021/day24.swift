b13bc4ae96f4776ac00eb25b61f113b2ebd0fc01
Ben Lings <ben.lings@gmail.com>
2021-12-24 15:56:28 +0000

    Parse day 24 input

--- Sources/AoC2021/day24.swift
+++ Sources/AoC2021/day24.swift
@@ -1,10 +1,99 @@
 import Foundation
 import AdventCore
 
-fileprivate let day24_input = Bundle.module.text(named: "day24").lines()
+//inp a - Read an input value and write it to variable a.
+//add a b - Add the value of a to the value of b, then store the result in variable a.
+//mul a b - Multiply the value of a by the value of b, then store the result in variable a.
+//div a b - Divide the value of a by the value of b, truncate the result to an integer, then store the result in variable a. (Here, "truncate" means to round the value toward zero.)
+//mod a b - Divide the value of a by the value of b, then store the remainder in variable a. (This is also called the modulo operation.)
+//eql a b - If the value of a and b are equal, then store the value 1 in variable a. Otherwise, store the value 0 in variable a.
+
+enum Variable : Character {
+    case w = "w", x = "x", y = "y", z = "z"
+}
+
+enum Value {
+    case literal(Int)
+    case variable(Variable)
+}
+
+enum BinaryOperation : String {
+    case add
+    case mul
+    case div
+    case mod
+    case eql
+}
+
+enum Instruction {
+    case inp(a: Variable)
+    case binary(op: BinaryOperation, a: Variable, b: Value)
+}
+
+extension Instruction {
+    init?(_ description: String) {
+        if let i = Scanner(string: description).scanInstruction() {
+            self = i
+        } else {
+            return nil
+        }
+    }
+}
+
+public struct ArithmeticLogicUnit {
+
+    var instructions: [Instruction]
+
+    var variables: [Variable : Int] = [
+        .w : 0,
+        .x : 0,
+        .y : 0,
+        .z : 0,
+    ]
+
+}
+
+extension ArithmeticLogicUnit {
+    public init(_ description: String) {
+        self.instructions = description.lines().compactMap(Instruction.init)
+    }
+}
+
+fileprivate extension Scanner {
+    func scanInstruction() -> Instruction? {
+        guard let name = scanUpToCharacters(from: .whitespaces) else { return nil }
+        switch name {
+        case "inp":
+            return scanVariable().map { .inp(a: $0) }
+        default:
+            guard let op = BinaryOperation(rawValue: name),
+                  let a = scanVariable(),
+                  let b = scanValue()
+            else { return nil }
+            return .binary(op: op, a: a, b: b)
+        }
+    }
+
+    func scanVariable() -> Variable? {
+        scanCharacter().flatMap { Variable(rawValue: $0) }
+    }
+
+    func scanValue() -> Value? {
+        if let literal = scanInt() {
+            return .literal(literal)
+        } else if let variable = scanVariable() {
+            return .variable(variable)
+        } else {
+            return nil
+        }
+    }
+}
+
+fileprivate let day24_input = Bundle.module.text(named: "day24")
 
 public func day24_1() -> Int {
-    0
+    _ = ArithmeticLogicUnit(day24_input)
+    return 0
 }
 
 public func day24_2() -> Int {
--- Tests/AoC2021Tests/day24.swift
+++ Tests/AoC2021Tests/day24.swift
@@ -4,9 +4,21 @@ import AoC2021
 final class Day24Tests: XCTestCase {
 
     let exampleInput = """
+    inp w
+    add z w
+    mod z 2
+    div w 2
+    add y w
+    mod y 2
+    div w 2
+    add x w
+    mod x 2
+    div w 2
+    mod w 2
     """
 
     func testPart1Example() {
+        var a = ArithmeticLogicUnit(exampleInput)
         XCTAssertEqual(0, 0)
     }
 

b562b82b25b41c231a59322873d0e6c888881b82
Ben Lings <ben.lings@gmail.com>
2022-12-01 06:58:00 +0000

    Update targets

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "AdventOfCode",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
+        .iOS(.v15),
+        .macOS(.v12),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -28,7 +28,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-collections", .branch("release/1.1")),
+        .package(url: "https://github.com/apple/swift-collections", branch: "release/1.1"),
         .package(url: "https://github.com/apple/swift-standard-library-preview.git", from: "0.0.1"),
     ],
     targets: [

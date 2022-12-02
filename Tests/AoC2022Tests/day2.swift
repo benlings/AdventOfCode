c7066980184fc505ebb5275ee66a9335b5243a66
Ben Lings <ben.lings@gmail.com>
2022-12-02 06:46:08 +0000

    Scaffold day 2

---
+++ Tests/AoC2022Tests/day2.swift
@@ -0,0 +1,24 @@
+import XCTest
+import AoC2022
+
+final class Day2Tests: XCTestCase {
+
+    let exampleInput = """
+    """
+
+    func testPart1Example() {
+        XCTAssertEqual(0, 0)
+    }
+
+    func testPart1() {
+        XCTAssertEqual(day2_1(), 0)
+    }
+
+    func testPart2Example() {
+        XCTAssertEqual(0, 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day2_2(), 0)
+    }
+}

21cee3b246999617ef678ed47ad479b3eb0f3376
Ben Lings <ben.lings@gmail.com>
2022-12-03 11:25:32 +0000

    Day 3 part 1

--- Sources/AoC2022/day3.swift
+++ Sources/AoC2022/day3.swift
@@ -1,10 +1,47 @@
 import Foundation
 import AdventCore
 
+public struct Rucksack {
+
+    public struct Item : Hashable {
+        var character: String.UTF8View.Element
+
+        var priority: Int {
+            switch character {
+            case 97...122: return Int(character - 96) // a-z = 1-26
+            case 65...90: return Int(character - 64 + 26) // A-Z = 27-52
+            default: fatalError()
+            }
+        }
+    }
+
+    var compartments: (Set<Item>, Set<Item>)
+
+    func errorItems() -> Set<Item> {
+        compartments.0.intersection(compartments.1)
+    }
+}
+
+public extension Rucksack {
+
+    init(_ line: String) {
+        let items = Array(line.utf8.map(Item.init(character:)))
+        compartments = (Set(items[0..<items.count/2]), Set(items[items.count/2..<items.count]))
+    }
+
+    static func sumErrorPriorities(_ lines: [String]) -> Int {
+        lines.map { line in
+            let r = Rucksack(line)
+            return r.errorItems().map(\.priority).sum()
+        }.sum()
+    }
+
+}
+
 fileprivate let day3_input = Bundle.module.text(named: "day3").lines()
 
 public func day3_1() -> Int {
-    0
+    Rucksack.sumErrorPriorities(day3_input)
 }
 
 public func day3_2() -> Int {
--- Tests/AoC2022Tests/day3.swift
+++ Tests/AoC2022Tests/day3.swift
@@ -4,14 +4,20 @@ import AoC2022
 final class Day3Tests: XCTestCase {
 
     let exampleInput = """
-    """
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
+    """.lines()
 
     func testPart1Example() {
-        XCTAssertEqual(0, 0)
+        XCTAssertEqual(Rucksack.sumErrorPriorities(exampleInput), 157)
     }
 
     func testPart1() {
-        XCTAssertEqual(day3_1(), 0)
+        XCTAssertEqual(day3_1(), 8240)
     }
 
     func testPart2Example() {

d95d53190a68052cc2d2e6ed0b7026ff4e8b1261
Ben Lings <ben.lings@gmail.com>
2022-12-04 12:22:10 +0000

    Day 4 part 1

--- Sources/AoC2022/day4.swift
+++ Sources/AoC2022/day4.swift
@@ -1,10 +1,45 @@
 import Foundation
 import AdventCore
 
+extension Scanner {
+    func scanClosedRange() -> ClosedRange<Int>? {
+        guard let lower = scanInt(),
+              let _ = scanString("-"),
+              let upper = scanInt() else {
+            return nil
+        }
+        return lower...upper
+    }
+}
+
+func parseRow(_ row: String) -> (ClosedRange<Int>, ClosedRange<Int>)? {
+    let scanner = Scanner(string: row)
+    guard let first = scanner.scanClosedRange(),
+          let _ = scanner.scanString(","),
+          let second = scanner.scanClosedRange() else {
+        return nil
+    }
+    return (first, second)
+}
+
+extension ClosedRange {
+
+    func contains(_ other: ClosedRange<Self.Bound>) -> Bool {
+        lowerBound <= other.lowerBound && upperBound >= other.upperBound
+    }
+
+}
+
+public func countOverlappingSections(_ input: [String]) -> Int {
+    input.compactMap(parseRow).count { sections in
+        sections.0.contains(sections.1) || sections.1.contains(sections.0)
+    }
+}
+
 fileprivate let day4_input = Bundle.module.text(named: "day4").lines()
 
 public func day4_1() -> Int {
-    0
+    countOverlappingSections(day4_input)
 }
 
 public func day4_2() -> Int {
--- Tests/AoC2022Tests/day4.swift
+++ Tests/AoC2022Tests/day4.swift
@@ -4,14 +4,20 @@ import AoC2022
 final class Day4Tests: XCTestCase {
 
     let exampleInput = """
-    """
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+    """.lines()
 
     func testPart1Example() {
-        XCTAssertEqual(0, 0)
+        XCTAssertEqual(countOverlappingSections(exampleInput), 2)
     }
 
     func testPart1() {
-        XCTAssertEqual(day4_1(), 0)
+        XCTAssertEqual(day4_1(), 562)
     }
 
     func testPart2Example() {

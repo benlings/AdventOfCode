d3422b85c551500b3928827891f28ca93f893b97
Ben Lings <ben.lings@gmail.com>
2022-12-04 12:04:46 +0000

    Scaffold day 4

---
+++ Sources/AoC2022/day4.swift
@@ -0,0 +1,12 @@
+import Foundation
+import AdventCore
+
+fileprivate let day4_input = Bundle.module.text(named: "day4").lines()
+
+public func day4_1() -> Int {
+    0
+}
+
+public func day4_2() -> Int {
+    0
+}
---
+++ Tests/AoC2022Tests/day4.swift
@@ -0,0 +1,24 @@
+import XCTest
+import AoC2022
+
+final class Day4Tests: XCTestCase {
+
+    let exampleInput = """
+    """
+
+    func testPart1Example() {
+        XCTAssertEqual(0, 0)
+    }
+
+    func testPart1() {
+        XCTAssertEqual(day4_1(), 0)
+    }
+
+    func testPart2Example() {
+        XCTAssertEqual(0, 0)
+    }
+
+    func testPart2() {
+        XCTAssertEqual(day4_2(), 0)
+    }
+}

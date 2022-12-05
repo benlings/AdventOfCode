b2c78f946b5c1192cca52804174cf37a98046911
Ben Lings <ben.lings@gmail.com>
2022-12-05 20:05:32 +0000

    Day 5

--- Sources/AoC2022/day5.swift
+++ Sources/AoC2022/day5.swift
@@ -1,12 +1,95 @@
 import Foundation
 import AdventCore
+import Collections
 
-fileprivate let day5_input = Bundle.module.text(named: "day5").lines()
+//     [D]
+// [N] [C]
+// [Z] [M] [P]
+//  1   2   3
+//
+// move 1 from 2 to 1
+// move 3 from 1 to 3
+// move 2 from 2 to 1
+// move 1 from 1 to 2
 
-public func day5_1() -> Int {
-    0
+struct Instruction {
+    var count: Int
+    var from: Int
+    var to: Int
+
+    init?(_ input: String) {
+        var scanner = Scanner(string: input)
+        guard let _ = scanner.scanString("move"),
+              let count = scanner.scanInt(),
+              let _ = scanner.scanString("from"),
+              let from = scanner.scanInt(),
+              let _ = scanner.scanString("to"),
+              let to = scanner.scanInt() else {
+            return nil
+        }
+        self.count = count
+        self.from = from - 1
+        self.to = to - 1
+    }
 }
 
-public func day5_2() -> Int {
-    0
+public struct Crates {
+
+    var stacks: [Deque<String>]
+
+    mutating func process(instructions: [Instruction], reverse: Bool) {
+        for i in instructions {
+            var items = stacks[i.from].prefix(i.count)
+            stacks[i.from].removeFirst(i.count)
+            if (reverse) {
+                items.reverse()
+            }
+            stacks[i.to].prepend(contentsOf: items)
+        }
+    }
+
+    var top: [String] {
+        stacks.compactMap { $0.first }
+    }
+
+}
+
+//"[Z] [M] [P]"
+
+public extension Crates {
+
+    init(_ input: String) {
+        let lines = input.lines().reversed()
+        stacks = Array(repeating: [], count: lines.first!.split(separator: " ").count)
+        for line in lines.dropFirst() {
+            let lineCharacters = Array(line)
+            for i in stacks.indices {
+                //01234567890123
+                //[Z] [M] [P]
+                let c = 1 + i * 4
+                if c <= lineCharacters.endIndex && lineCharacters[c] != " " {
+                    stacks[i].prepend(String(lineCharacters[c]))
+                }
+            }
+        }
+    }
+
+    static func solve(_ input: String, reverse: Bool = true) -> String {
+        let sections = input.groups()
+        var crates = Crates(sections[0])
+        let instructions = sections[1].lines().compactMap(Instruction.init)
+        crates.process(instructions: instructions, reverse: reverse)
+        return crates.top.joined()
+    }
+}
+
+
+fileprivate let day5_input = Bundle.module.text(named: "day5")
+
+public func day5_1() -> String {
+    Crates.solve(day5_input)
+}
+
+public func day5_2() -> String {
+    Crates.solve(day5_input, reverse: false)
 }
--- Tests/AoC2022Tests/day5.swift
+++ Tests/AoC2022Tests/day5.swift
@@ -4,21 +4,30 @@ import AoC2022
 final class Day5Tests: XCTestCase {
 
     let exampleInput = """
+        [D]
+    [N] [C]
+    [Z] [M] [P]
+     1   2   3
+
+    move 1 from 2 to 1
+    move 3 from 1 to 3
+    move 2 from 2 to 1
+    move 1 from 1 to 2
     """
 
     func testPart1Example() {
-        XCTAssertEqual(0, 0)
+        XCTAssertEqual(Crates.solve(exampleInput), "CMZ")
     }
 
     func testPart1() {
-        XCTAssertEqual(day5_1(), 0)
+        XCTAssertEqual(day5_1(), "TGWSMRBPN")
     }
 
     func testPart2Example() {
-        XCTAssertEqual(0, 0)
+        XCTAssertEqual(Crates.solve(exampleInput, reverse: false), "MCD")
     }
 
     func testPart2() {
-        XCTAssertEqual(day5_2(), 0)
+        XCTAssertEqual(day5_2(), "TZLTLWRNF")
     }
 }

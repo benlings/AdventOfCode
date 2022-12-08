c44c79dd9d207ae72399683c691f90cfce332c33
Ben Lings <ben.lings@gmail.com>
2022-12-08 22:14:07 +0000

    Scaffold day 8

---
+++ Sources/AoC2022/day8.swift
@@ -0,0 +1,12 @@
+import Foundation
+import AdventCore
+
+fileprivate let day8_input = Bundle.module.text(named: "day8").lines()
+
+public func day8_1() -> Int {
+    0
+}
+
+public func day8_2() -> Int {
+    0
+}

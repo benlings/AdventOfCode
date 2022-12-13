6d3f6791c425561a201354dd421e70903eb37eea
Ben Lings <ben.lings@gmail.com>
2022-12-13 22:23:21 +0000

    Day 13 part 1

--- Sources/AoC2022/day13.swift
+++ Sources/AoC2022/day13.swift
@@ -1,10 +1,93 @@
 import Foundation
 import AdventCore
+import SE0270_RangeSet
 
-fileprivate let day13_input = Bundle.module.text(named: "day13").lines()
+fileprivate extension Scanner {
+    func scanPacketData() -> PacketData? {
+        let i = currentIndex
+        if let _ = scanString("[") {
+            var contents = [PacketData]()
+            while let d = scanPacketData() {
+                contents.append(d)
+                if scanString("]") != nil {
+                    break
+                }
+                _ = scanString(",")
+            }
+            return .list(contents)
+        } else if let i = scanInt() {
+            return .integer(i)
+        } else {
+            return nil
+        }
+    }
+}
+
+enum PacketData {
+    case integer(Int)
+    case list([PacketData])
+}
+
+extension PacketData {
+    init?(_ input: String) {
+        let scanner = Scanner(string: input)
+        guard let s = scanner.scanPacketData() else { return nil }
+        self = s
+    }
+}
+
+extension PacketData : Comparable {
+    static func < (lhs: PacketData, rhs: PacketData) -> Bool {
+        switch (lhs, rhs) {
+        case (.integer(let l), .integer(let r)): return l < r
+        case (.list(let l), .list(let r)):
+            for i in 0..<(max(l.count, r.count)) {
+                if i >= l.count {
+                    return true
+                }
+                if i >= r.count {
+                    return false
+                }
+                if l[i] < r[i] {
+                    return true
+                }
+                if l[i] > r[i] {
+                    return false
+                }
+            }
+            return false
+        case (.integer(let l), .list(let r)): return .list([.integer(l)]) < .list(r)
+        case (.list(let l), .integer(let r)): return .list(l) < .list([.integer(r)])
+        }
+    }
+}
+
+public struct DistressSignal {
+
+    var packets: [(PacketData, PacketData)]
+
+    public func sumRightOrderIndexes() -> Int {
+        packets.enumerated()
+            .filter { $0.element.0 < $0.element.1 }
+            .map { $0.offset + 1 }
+            .sum()
+    }
+}
+
+public extension DistressSignal {
+    init(_ input: String) {
+        packets = input.groups().map { group in
+            let packets = group.lines().compactMap(PacketData.init)
+            return (packets[0], packets[1])
+        }
+        
+    }
+}
+
+fileprivate let day13_input = Bundle.module.text(named: "day13")
 
 public func day13_1() -> Int {
-    0
+    DistressSignal(day13_input).sumRightOrderIndexes()
 }
 
 public func day13_2() -> Int {
--- Tests/AoC2022Tests/day13.swift
+++ Tests/AoC2022Tests/day13.swift
@@ -4,14 +4,37 @@ import AoC2022
 final class Day13Tests: XCTestCase {
 
     let exampleInput = """
+    [1,1,3,1,1]
+    [1,1,5,1,1]
+
+    [[1],[2,3,4]]
+    [[1],4]
+
+    [9]
+    [[8,7,6]]
+
+    [[4,4],4,4]
+    [[4,4],4,4,4]
+
+    [7,7,7,7]
+    [7,7,7]
+
+    []
+    [3]
+
+    [[[]]]
+    [[]]
+
+    [1,[2,[3,[4,[5,6,7]]]],8,9]
+    [1,[2,[3,[4,[5,6,0]]]],8,9]
     """
 
     func testPart1Example() {
-        XCTAssertEqual(0, 0)
+        XCTAssertEqual(DistressSignal(exampleInput).sumRightOrderIndexes(), 13)
     }
 
     func testPart1() {
-        XCTAssertEqual(day13_1(), 0)
+        XCTAssertEqual(day13_1(), 5198)
     }
 
     func testPart2Example() {

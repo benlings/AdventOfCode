f7bd030ccb88edfb51c6fcdb5a29a3b6fbdd3edc
Ben Lings <ben.lings@gmail.com>
2022-12-15 19:30:20 +0000

    Day 15 part 1

--- Sources/AoC2022/day15.swift
+++ Sources/AoC2022/day15.swift
@@ -1,10 +1,57 @@
 import Foundation
 import AdventCore
 
+public struct BeaconSensor {
+    var sensor: Offset
+    var closestBeacon: Offset
+
+    var distance: Int { sensor.manhattanDistance(to: closestBeacon) }
+
+    var rowRange: ClosedRange<Int> { (sensor.north - distance)...(sensor.north + distance) }
+
+    func columnRange(row: Int) -> ClosedRange<Int> {
+        let extent = distance - abs(sensor.north - row)
+        return (sensor.east - extent)...(sensor.east + extent)
+    }
+
+    public static func countNonBeacons(_ input: some Sequence<String>, row: Int) -> Int {
+        let sensors = input.compactMap(Self.init).filter { $0.rowRange.contains(row) }
+        let beacons = sensors.filter { $0.closestBeacon.north == row }
+        var nonBeacons = Set<Int>()
+        for sensor in sensors {
+            nonBeacons.formUnion(sensor.columnRange(row: row))
+        }
+        for sensor in beacons {
+            nonBeacons.remove(sensor.closestBeacon.east)
+        }
+        return nonBeacons.count
+    }
+}
+
+// Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+
+extension BeaconSensor {
+    init?(_ input: String) {
+        let scanner = Scanner(string: input)
+        guard let _ = scanner.scanString("Sensor at x="),
+              let x = scanner.scanInt(),
+              let _ = scanner.scanString(", y="),
+              let y = scanner.scanInt(),
+              let _ = scanner.scanString(": closest beacon is at x="),
+              let beaconX = scanner.scanInt(),
+              let _ = scanner.scanString(", y="),
+              let beaconY = scanner.scanInt()
+        else { return nil }
+        sensor = Offset(east: x, north: y)
+        closestBeacon = Offset(east: beaconX, north: beaconY)
+    }
+}
+
+
 fileprivate let day15_input = Bundle.module.text(named: "day15").lines()
 
 public func day15_1() -> Int {
-    0
+    BeaconSensor.countNonBeacons(day15_input, row: 2000000)
 }
 
 public func day15_2() -> Int {
--- Tests/AoC2022Tests/day15.swift
+++ Tests/AoC2022Tests/day15.swift
@@ -4,14 +4,28 @@ import AoC2022
 final class Day15Tests: XCTestCase {
 
     let exampleInput = """
-    """
+    Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+    Sensor at x=9, y=16: closest beacon is at x=10, y=16
+    Sensor at x=13, y=2: closest beacon is at x=15, y=3
+    Sensor at x=12, y=14: closest beacon is at x=10, y=16
+    Sensor at x=10, y=20: closest beacon is at x=10, y=16
+    Sensor at x=14, y=17: closest beacon is at x=10, y=16
+    Sensor at x=8, y=7: closest beacon is at x=2, y=10
+    Sensor at x=2, y=0: closest beacon is at x=2, y=10
+    Sensor at x=0, y=11: closest beacon is at x=2, y=10
+    Sensor at x=20, y=14: closest beacon is at x=25, y=17
+    Sensor at x=17, y=20: closest beacon is at x=21, y=22
+    Sensor at x=16, y=7: closest beacon is at x=15, y=3
+    Sensor at x=14, y=3: closest beacon is at x=15, y=3
+    Sensor at x=20, y=1: closest beacon is at x=15, y=3
+    """.lines()
 
     func testPart1Example() {
-        XCTAssertEqual(0, 0)
+        XCTAssertEqual(BeaconSensor.countNonBeacons(exampleInput, row: 10), 26)
     }
 
     func testPart1() {
-        XCTAssertEqual(day15_1(), 0)
+        XCTAssertEqual(day15_1(), 4717631)
     }
 
     func testPart2Example() {

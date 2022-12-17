32659a89401689f3852878104028898e7adf6117
Ben Lings <ben.lings@gmail.com>
2022-12-17 20:43:15 +0000

    Day 16 part 1

--- Sources/AoC2022/day16.swift
+++ Sources/AoC2022/day16.swift
@@ -1,10 +1,105 @@
 import Foundation
 import AdventCore
+import Collections
+
+struct ValveState : Hashable {
+    var currentValve: String = "AA"
+    var flowRates: [String : Int]
+    var remaining: Int = 30
+
+    var currentFlowRate: Int {
+        get { flowRates[currentValve]! }
+        set { flowRates[currentValve] = newValue }
+    }
+
+    mutating func turnOnValve() -> Int? {
+        let rate = currentFlowRate
+        guard currentFlowRate > 0 else { return nil }
+        remaining -= 1
+        currentFlowRate = 0
+        return rate * remaining
+    }
+
+    mutating func moveTo(valve: String) {
+        currentValve = valve
+        remaining -= 1
+    }
+}
+
+public struct ValveScan {
+
+    var tunnels: [String : [String]]
+    var startingState: ValveState
+
+    public func maxPressureRelease() -> Int {
+        findLargestRelease(start: startingState)!
+    }
+
+    func findNeighbourStates(start: ValveState) -> [(release: Int, ValveState)] {
+        var neighbours = [(Int, ValveState)]()
+        var turnedOn = start
+        if let release = turnedOn.turnOnValve(), turnedOn.remaining >= 0 {
+            neighbours.append((release, turnedOn))
+        }
+
+        for exit in tunnels[start.currentValve]! {
+            var next = start
+            var prev = next.currentValve
+            next.moveTo(valve: exit)
+            // Follow tunnels with no choices (another exit or valve to turn on)
+            while next.currentFlowRate == 0 && tunnels[next.currentValve]!.count == 2 {
+                let following = tunnels[next.currentValve]!.first { $0 != prev }!
+                prev = next.currentValve
+                next.moveTo(valve: following)
+            }
+            if next.remaining >= 0 {
+                neighbours.append((0, next))
+            }
+        }
+        return neighbours
+    }
+
+    func findLargestRelease(start: ValveState) -> Int? {
+        var release = [start: 0]
+        var toVisit = [start] as Deque<ValveState>
+        while let current = toVisit.popFirst() {
+            let currentRelease = release[current]!
+            for (neighbourRelease, neighbour) in findNeighbourStates(start: current) {
+                let alt = currentRelease + neighbourRelease
+                if alt > release[neighbour, default: .min] {
+                    release[neighbour] = alt
+                    toVisit.append(neighbour)
+                }
+            }
+        }
+        return release.values.max()
+    }
+
+}
+
+public extension ValveScan {
+    init(_ input: some Sequence<String>) {
+        startingState = ValveState(flowRates: [:])
+        tunnels = [:]
+        for line in input {
+            let scanner = Scanner(string: line)
+            guard let _ = scanner.scanString("Valve"),
+                  let valve = scanner.scanCharacters(from: .uppercaseLetters),
+                  let _ = scanner.scanString("has flow rate="),
+                  let rate = scanner.scanInt(),
+                  scanner.scanString("; tunnels lead to valves") != nil || scanner.scanString("; tunnel leads to valve") != nil,
+                  case let dest = scanner.scanSequence(separator: ",", scanElement: { $0.scanCharacters(from: .uppercaseLetters) })
+            else { continue }
+            startingState.flowRates[valve] = rate
+            tunnels[valve] = dest
+        }
+    }
+}
 
 fileprivate let day16_input = Bundle.module.text(named: "day16").lines()
 
 public func day16_1() -> Int {
-    0
+    ValveScan(day16_input).maxPressureRelease()
 }
 
 public func day16_2() -> Int {
--- Tests/AoC2022Tests/day16.swift
+++ Tests/AoC2022Tests/day16.swift
@@ -4,14 +4,24 @@ import AoC2022
 final class Day16Tests: XCTestCase {
 
     let exampleInput = """
-    """
+    Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+    Valve BB has flow rate=13; tunnels lead to valves CC, AA
+    Valve CC has flow rate=2; tunnels lead to valves DD, BB
+    Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+    Valve EE has flow rate=3; tunnels lead to valves FF, DD
+    Valve FF has flow rate=0; tunnels lead to valves EE, GG
+    Valve GG has flow rate=0; tunnels lead to valves FF, HH
+    Valve HH has flow rate=22; tunnel leads to valve GG
+    Valve II has flow rate=0; tunnels lead to valves AA, JJ
+    Valve JJ has flow rate=21; tunnel leads to valve II
+    """.lines()
 
     func testPart1Example() {
-        XCTAssertEqual(0, 0)
+        XCTAssertEqual(ValveScan(exampleInput).maxPressureRelease(), 1651)
     }
 
     func testPart1() {
-        XCTAssertEqual(day16_1(), 0)
+        XCTAssertEqual(day16_1(), 2181)
     }
 
     func testPart2Example() {

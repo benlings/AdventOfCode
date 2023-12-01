fb5784a643719f93a35c238f2c712989e57ffaf9
Ben Lings <ben.lings@bp.com>
2023-12-01 21:42:16 +0000

    Solve part 2

--- Sources/AoC2023/day1.swift
+++ Sources/AoC2023/day1.swift
@@ -2,10 +2,43 @@ import Foundation
 import AdventCore
 
 func calibrationValue(_ line: String) -> Int? {
-  let digits = line.filter(\.isNumber)
-  guard let first = digits.first,
-        let last = digits.last else { return nil }
+  guard let first = line.first(where: \.isNumber),
+        let last = line.last(where: \.isNumber) else { return nil }
   return Int(String([first, last]))
+}
+
+let digitValues = [
+  "1": 1,
+  "2": 2,
+  "3": 3,
+  "4": 4,
+  "5": 5,
+  "6": 6,
+  "7": 7,
+  "8": 8,
+  "9": 9,
+  "one": 1,
+  "two": 2,
+  "three": 3,
+  "four": 4,
+  "five": 5,
+  "six": 6,
+  "seven": 7,
+  "eight": 8,
+  "nine": 9,
+]
+
+let digitRegex = try! Regex("\(digitValues.keys.joined(separator: "|"))")
+let reverseDigitRegex = try! Regex("\(String(digitValues.keys.map { $0.reversed() }.joined(separator: "|"))))")
+
+func calibrationValue2(_ line: String) -> Int? {
+  if let firstD = line.firstMatch(of: digitRegex)?.output[0].substring,
+     let firstN = digitValues[String(firstD)],
+     let lastD = String(line.reversed()).firstMatch(of: reverseDigitRegex)?.output[0].substring,
+     let lastN = digitValues[String(lastD.reversed())] {
+    return firstN * 10 + lastN
+  }
+  return nil
 }
 
 public func day1_1(_ input: String) -> Int {
@@ -15,6 +48,9 @@ public func day1_1(_ input: String) -> Int {
     .sum()
 }
 
-public func day1_2(_ intput: String) -> Int {
-    0
+public func day1_2(_ input: String) -> Int {
+  input
+    .lines()
+    .compactMap(calibrationValue2)
+    .sum()
 }
--- Tests/AoC2023Tests/day1.swift
+++ Tests/AoC2023Tests/day1.swift
@@ -20,11 +20,21 @@ final class Day1Tests: XCTestCase {
         XCTAssertEqual(day1_1(input), 55488)
     }
 
+    let exampleInput2 = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
     func testPart2Example() {
-        XCTAssertEqual(day1_2(exampleInput), 0)
+        XCTAssertEqual(day1_2(exampleInput2), 281)
     }
 
     func testPart2() {
-        XCTAssertEqual(day1_2(input), 0)
+        XCTAssertEqual(day1_2(input), 55614)
     }
 }

8e9d6271d65726cb87c3486a73fbd94e814895ac
Ben Lings <ben.lings@bp.com>
2023-12-01 20:40:50 +0000

    Implement part 1

--- Sources/AoC2023/day1.swift
+++ Sources/AoC2023/day1.swift
@@ -1,8 +1,18 @@
 import Foundation
 import AdventCore
 
+func calibrationValue(_ line: String) -> Int? {
+  let digits = line.filter(\.isNumber)
+  guard let first = digits.first,
+        let last = digits.last else { return nil }
+  return Int(String([first, last]))
+}
+
 public func day1_1(_ input: String) -> Int {
-    0
+  input
+    .lines()
+    .compactMap(calibrationValue)
+    .sum()
 }
 
 public func day1_2(_ intput: String) -> Int {
--- Tests/AoC2023Tests/day1.swift
+++ Tests/AoC2023Tests/day1.swift
@@ -6,14 +6,18 @@ final class Day1Tests: XCTestCase {
     let input = Bundle.module.text(named: "day1")
 
     let exampleInput = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
     """
 
     func testPart1Example() {
-        XCTAssertEqual(day1_1(exampleInput), 0)
+        XCTAssertEqual(day1_1(exampleInput), 142)
     }
 
     func testPart1() {
-        XCTAssertEqual(day1_1(input), 0)
+        XCTAssertEqual(day1_1(input), 55488)
     }
 
     func testPart2Example() {

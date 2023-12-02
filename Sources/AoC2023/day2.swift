4fdcea519eb98c3b93d5aa34f9e07c0146258693
Ben Lings <ben.lings@bp.com>
2023-12-02 11:20:12 +0000

    Day 2 part 1

--- Sources/AoC2023/day2.swift
+++ Sources/AoC2023/day2.swift
@@ -1,8 +1,65 @@
 import Foundation
 import AdventCore
 
+struct GameRecord {
+  var id: Int
+  var revealedCubes: [[String: Int]]
+}
+
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+extension Scanner {
+  
+  func scanCubes() -> [String: Int] {
+    var cubes = [String: Int]()
+    while !isAtEnd, scanString(";") == nil {
+      if let num = scanInt(),
+         let colour = scanUpToCharacters(from: CharacterSet(charactersIn: ",;")) {
+        cubes[colour] = num
+        _ = scanString(",")
+      }
+    }
+    return cubes
+  }
+
+  func scanRevealtedCubes() -> [[String: Int]] {
+    var reveledCubes = [[String: Int]]()
+    while !isAtEnd {
+      let cubes = scanCubes()
+      if !cubes.isEmpty {
+        reveledCubes.append(cubes)
+      }
+      _ = scanString(";")
+    }
+    return reveledCubes
+  }
+}
+
+extension GameRecord {
+  init?(_ description: String) {
+    let scanner = Scanner(string: description)
+    guard let _ = scanner.scanString("Game"),
+          let id = scanner.scanInt(),
+          let _ = scanner.scanString(":")
+    else { return nil }
+    self.id = id
+    self.revealedCubes = scanner.scanRevealtedCubes()
+  }
+
+  var isPossible: Bool {
+    revealedCubes.allSatisfy(Self.isPossible(cubes:))
+  }
+
+  static func isPossible(cubes: [String: Int]) -> Bool {
+    cubes["red", default: 0] <= 12 &&
+    cubes["green", default: 0] <= 13 &&
+    cubes["blue", default: 0] <= 14
+  }
+}
+
+// The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
 public func day2_1(_ input: String) -> Int {
-    0
+  input.lines().compactMap(GameRecord.init).filter(\.isPossible).map(\.id).sum()
 }
 
 public func day2_2(_ input: String) -> Int {
--- Tests/AoC2023Tests/day2.swift
+++ Tests/AoC2023Tests/day2.swift
@@ -6,14 +6,19 @@ final class Day2Tests: XCTestCase {
     let input = Bundle.module.text(named: "day2")
 
     let exampleInput = """
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     """
 
     func testPart1Example() {
-        XCTAssertEqual(day2_1(exampleInput), 0)
+        XCTAssertEqual(day2_1(exampleInput), 8)
     }
 
     func testPart1() {
-        XCTAssertEqual(day2_1(input), 0)
+        XCTAssertEqual(day2_1(input), 3059)
     }
 
     func testPart2Example() {

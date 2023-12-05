863cd49f9fee7061be5b8e4e8981aac7105e2b42
Ben Lings <ben.lings@bp.com>
2023-12-05 08:06:31 +0000

    Day 5 part 1

--- Sources/AoC2023/day5.swift
+++ Sources/AoC2023/day5.swift
@@ -1,8 +1,95 @@
 import Foundation
 import AdventCore
 
+//seeds: 79 14 55 13
+//
+//seed-to-soil map:
+//50 98 2
+//52 50 48
+//
+//soil-to-fertilizer map:
+//0 15 37
+//37 52 2
+//39 0 15
+//
+//fertilizer-to-water map:
+//49 53 8
+//0 11 42
+//42 0 7
+//57 7 4
+//
+//water-to-light map:
+//88 18 7
+//18 25 70
+//
+//light-to-temperature map:
+//45 77 23
+//81 45 19
+//68 64 13
+//
+//temperature-to-humidity map:
+//0 69 1
+//1 0 69
+//
+//humidity-to-location map:
+//60 56 37
+//56 93 4
+
+struct RangeMap {
+  struct Entry {
+    var destinationStart: Int
+    var sourceStart: Int
+    var length: Int
+    private var sourceRange: Range<Int> { sourceStart..<(sourceStart + length) }
+    func contains(_ source: Int) -> Bool {
+      sourceRange.contains(source)
+    }
+    subscript(source: Int) -> Int {
+      assert(sourceRange.contains(source))
+      return source - sourceStart + destinationStart
+    }
+  }
+
+  var entries: [Entry]
+
+  subscript(source: Int) -> Int {
+    if let entry = entries.first(where: { $0.contains(source) }) {
+      return entry[source]
+    }
+    return source
+  }
+}
+
+extension Scanner {
+  // destStart sourceStart length
+  // 50 98 2
+  func scanRangeMapEntry() -> RangeMap.Entry? {
+    guard let destStart = scanInt(),
+          let sourceStart = scanInt(),
+          let length = scanInt()
+    else { return nil }
+    return RangeMap.Entry(destinationStart: destStart, sourceStart: sourceStart, length: length)
+  }
+}
+
 public func day5_1(_ input: String) -> Int {
-  0
+  var groups = input.groups()
+  let seeds = groups.removeFirst().trimmingPrefix(#/seeds: /#).split(separator: #/\s+/#).compactMap { Int($0) }
+  let maps = groups.map { group in
+    var entries = group.lines()
+    _ = entries.removeFirst() // Ignore spec on entry types - they are in the right order to just pass through
+    return RangeMap(
+      entries: entries.compactMap {
+        let s = Scanner(string: $0)
+        return s.scanRangeMapEntry()
+      }
+    )
+  }
+  return seeds.map { seed in
+    maps.reduce(seed) { source, map in
+      map[source]
+    }
+  }.min() ?? 0
 }
 
 public func day5_2(_ input: String) -> Int {
--- Tests/AoC2023Tests/day5.swift
+++ Tests/AoC2023Tests/day5.swift
@@ -6,14 +6,47 @@ final class Day5Tests: XCTestCase {
   let input = Bundle.module.text(named: "day5")
 
   let exampleInput = """
+  seeds: 79 14 55 13
+
+  seed-to-soil map:
+  50 98 2
+  52 50 48
+
+  soil-to-fertilizer map:
+  0 15 37
+  37 52 2
+  39 0 15
+
+  fertilizer-to-water map:
+  49 53 8
+  0 11 42
+  42 0 7
+  57 7 4
+
+  water-to-light map:
+  88 18 7
+  18 25 70
+
+  light-to-temperature map:
+  45 77 23
+  81 45 19
+  68 64 13
+
+  temperature-to-humidity map:
+  0 69 1
+  1 0 69
+
+  humidity-to-location map:
+  60 56 37
+  56 93 4
   """
 
   func testPart1Example() {
-    XCTAssertEqual(day5_1(exampleInput), 0)
+    XCTAssertEqual(day5_1(exampleInput), 35)
   }
 
   func testPart1() {
-    XCTAssertEqual(day5_1(input), 0)
+    XCTAssertEqual(day5_1(input), 278755257)
   }
 
   func testPart2Example() {

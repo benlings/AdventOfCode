47e2568a062443881f013416f38fd789663cebe9
Ben Lings <ben.lings@bp.com>
2023-12-07 09:49:39 +0000

    Day 7 part 1

--- Sources/AoC2023/day7.swift
+++ Sources/AoC2023/day7.swift
@@ -1,8 +1,127 @@
 import Foundation
 import AdventCore
 
+//  Five of a kind, where all five cards have the same label: AAAAA
+//  Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+//  Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+//  Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+//  Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+//  One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+//  High card, where all cards' labels are distinct: 23456
+
+enum HandType: Comparable {
+  case highCard
+  case onePair
+  case twoPair
+  case threeOfAKind
+  case fullHouse
+  case fourOfAKind
+  case fiveOfAKind
+
+  init(cards: [Hand.Card]) {
+    assert(cards.count == 5)
+    let counts = cards.grouped(by: { $0 }).mapValues(\.count)
+    switch counts.count {
+    case 1:
+      self = .fiveOfAKind
+    case 2:
+      if counts.values.contains(where: { $0 == 4 }) {
+        self = .fourOfAKind
+      } else {
+        self = .fullHouse
+      }
+    case 3:
+      if counts.values.contains(where: { $0 == 3 }) {
+        self = .threeOfAKind
+      } else {
+        self = .twoPair
+      }
+    case 4:
+      self = .onePair
+    case 5:
+      self = .highCard
+    default:
+      fatalError()
+    }
+  }
+}
+
+struct Hand {
+
+  enum Card: Character, Comparable {
+    case ace = "A"
+    case king = "K"
+    case queen = "Q"
+    case jack = "J"
+    case ten = "T"
+    case nine = "9"
+    case eight = "8"
+    case seven = "7"
+    case six = "6"
+    case five = "5"
+    case four = "4"
+    case three = "3"
+    case two = "2"
+
+    private var comparisonValue: Int {
+      switch self {
+      case .ace: 13
+      case .king: 12
+      case .queen: 11
+      case .jack: 10
+      case .ten: 9
+      case .nine: 8
+      case .eight: 7
+      case .seven: 6
+      case .six: 5
+      case .five: 4
+      case .four: 3
+      case .three: 2
+      case .two: 1
+      }
+    }
+
+    static func < (lhs: Self, rhs: Self) -> Bool {
+      return lhs.comparisonValue < rhs.comparisonValue
+    }
+  }
+
+  var cards: [Card]
+  var type: HandType
+
+  var bid: Int
+
+  init(cards: [Card], bid: Int) {
+    assert(cards.count == 5)
+    self.cards = cards
+    self.type = HandType(cards: cards)
+    self.bid = bid
+  }
+}
+
+extension Hand {
+  init?(_ description: String) {
+    let scanner = Scanner(string: description)
+    guard let cards = scanner.scanUpToCharacters(from: .whitespaces),
+          let bid = scanner.scanInt()
+    else { return nil }
+    self = Hand(cards: cards.compactMap(Hand.Card.init), bid: bid)
+  }
+}
+
 public func day7_1(_ input: String) -> Int {
-  0
+  input
+    .lines()
+    .compactMap(Hand.init)
+    .sorted { lhs, rhs in
+      if lhs.type == rhs.type {
+        return lhs.cards.lexicographicallyPrecedes(rhs.cards)
+      }
+      return lhs.type < rhs.type
+    }
+    .enumerated()
+    .map { (index, hand) in (index + 1) * hand.bid }
+    .sum()
 }
 
 public func day7_2(_ input: String) -> Int {
--- Tests/AoC2023Tests/day7.swift
+++ Tests/AoC2023Tests/day7.swift
@@ -6,10 +6,15 @@ final class Day7Tests: XCTestCase {
   let input = Bundle.module.text(named: "day7")
 
   let exampleInput = """
+  32T3K 765
+  T55J5 684
+  KK677 28
+  KTJJT 220
+  QQQJA 483
   """
 
   func testPart1Example() {
-    XCTAssertEqual(day7_1(exampleInput), 0)
+    XCTAssertEqual(day7_1(exampleInput), 6440)
   }
 
   func testPart1() {

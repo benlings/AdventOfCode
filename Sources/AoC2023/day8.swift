7e69de13228c53762592c396fd0fbb36aa05c31e
Ben Lings <ben.lings@bp.com>
2023-12-08 08:01:15 +0000

    Day 8 part 1

--- Sources/AoC2023/day8.swift
+++ Sources/AoC2023/day8.swift
@@ -1,8 +1,66 @@
 import Foundation
 import AdventCore
 
+enum Direction: Character {
+  case left = "L"
+  case right = "R"
+  func pick(_ nodes: (left: Network.ID, right: Network.ID)) -> Network.ID {
+    switch self {
+    case .left: nodes.left
+    case .right: nodes.right
+    }
+  }
+}
+
+struct Network {
+  typealias ID = String
+  var instructions: [Direction]
+  var nodes: [ID: (left: ID, right: ID)]
+  static let start: ID = "AAA"
+  static let end: ID = "ZZZ"
+
+  func countSteps() -> Int {
+    var count = 0
+    var node = Self.start
+    for instruction in instructions.cycled() {
+      if node == Self.end {
+        break
+      }
+      node = instruction.pick(nodes[node]!)
+      count += 1
+    }
+    return count
+  }
+}
+
+extension Scanner {
+  func scanNetworkNode() -> (start: Network.ID, end: (left: Network.ID, right: Network.ID))? {
+    guard let start = scanUpToCharacters(from: .whitespaces),
+          let _ = scanString("= ("),
+          let left = scanUpToString(","),
+          let _ = scanString(","),
+          let right = scanUpToString(")"),
+          let _ = scanString(")")
+    else { return nil }
+    return (start, (left, right))
+  }
+}
+
+extension Network {
+  init(_ description: String) {
+    let groups = description.groups()
+    self.instructions = groups[0].compactMap(Direction.init)
+    self.nodes = [:]
+    let scanner = Scanner(string: groups[1])
+    while !scanner.isAtEnd, let node = scanner.scanNetworkNode() {
+      self.nodes[node.start] = node.end
+    }
+  }
+}
+
 public func day8_1(_ input: String) -> Int {
-  0
+  let network = Network(input)
+  return network.countSteps()
 }
 
 public func day8_2(_ input: String) -> Int {
--- Tests/AoC2023Tests/day8.swift
+++ Tests/AoC2023Tests/day8.swift
@@ -5,19 +5,43 @@ final class Day8Tests: XCTestCase {
 
   let input = Bundle.module.text(named: "day8")
 
-  let exampleInput = """
+  let exampleInput1 = """
+  RL
+
+  AAA = (BBB, CCC)
+  BBB = (DDD, EEE)
+  CCC = (ZZZ, GGG)
+  DDD = (DDD, DDD)
+  EEE = (EEE, EEE)
+  GGG = (GGG, GGG)
+  ZZZ = (ZZZ, ZZZ)
   """
 
-  func testPart1Example() {
-    XCTAssertEqual(day8_1(exampleInput), 0)
+  func testPart1Example1() {
+    XCTAssertEqual(day8_1(exampleInput1), 2)
+  }
+
+  let exampleInput2 = """
+  LLR
+
+  AAA = (BBB, BBB)
+  BBB = (AAA, ZZZ)
+  ZZZ = (ZZZ, ZZZ)
+  """
+
+  func testPart1Example2() {
+    XCTAssertEqual(day8_1(exampleInput2), 6)
   }
 
   func testPart1() {
-    XCTAssertEqual(day8_1(input), 0)
+    XCTAssertEqual(day8_1(input), 11309)
   }
 
+  let exampleInput3 = """
+  """
+
   func testPart2Example() {
-    XCTAssertEqual(day8_2(exampleInput), 0)
+    XCTAssertEqual(day8_2(exampleInput3), 6)
   }
 
   func testPart2() {

902ade7b5829bdb29cd225a1c152c7f211d125e2
Ben Lings <ben.lings@bp.com>
2023-12-09 11:27:02 +0000

    Day 9 part 1

--- Sources/AoC2023/day9.swift
+++ Sources/AoC2023/day9.swift
@@ -1,8 +1,27 @@
 import Foundation
 import AdventCore
 
+func differences(_ input: String) -> [[Int]] {
+  var numbers = input.split(separator: " ").compactMap { Int(String($0)) }
+  var differences = [numbers]
+  while !numbers.allSatisfy({ $0 == 0 }) {
+    numbers = numbers.adjacentPairs().map { $0.1 - $0.0 }
+    differences.append(numbers)
+  }
+  return differences
+}
+
+func propagate(_ differences: [[Int]]) -> Int {
+  differences.compactMap(\.last).sum()
+}
+
 public func day9_1(_ input: String) -> Int {
-  0
+  input
+    .lines()
+    .map {
+      propagate(differences($0))
+    }
+    .sum()
 }
 
 public func day9_2(_ input: String) -> Int {
--- Tests/AoC2023Tests/day9.swift
+++ Tests/AoC2023Tests/day9.swift
@@ -6,14 +6,17 @@ final class Day9Tests: XCTestCase {
   let input = Bundle.module.text(named: "day9")
 
   let exampleInput = """
+  0 3 6 9 12 15
+  1 3 6 10 15 21
+  10 13 16 21 30 45
   """
 
   func testPart1Example() {
-    XCTAssertEqual(day9_1(exampleInput), 0)
+    XCTAssertEqual(day9_1(exampleInput), 114)
   }
 
   func testPart1() {
-    XCTAssertEqual(day9_1(input), 0)
+    XCTAssertEqual(day9_1(input), 2175229206)
   }
 
   func testPart2Example() {

7d79b2a78c0d66a7557ed41cc796511d75f98862
Ben Lings <ben.lings@bp.com>
2023-12-11 07:29:51 +0000

    Scaffold day 11

---
+++ Sources/AoC2023/day11.swift
@@ -0,0 +1,10 @@
+import Foundation
+import AdventCore
+
+public func day11_1(_ input: String) -> Int {
+  0
+}
+
+public func day11_2(_ input: String) -> Int {
+  0
+}
---
+++ Tests/AoC2023Tests/day11.swift
@@ -0,0 +1,26 @@
+import XCTest
+import AoC2023
+
+final class Day11Tests: XCTestCase {
+
+  let input = Bundle.module.text(named: "day11")
+
+  let exampleInput = """
+  """
+
+  func testPart1Example() {
+    XCTAssertEqual(day11_1(exampleInput), 0)
+  }
+
+  func testPart1() {
+    XCTAssertEqual(day11_1(input), 0)
+  }
+
+  func testPart2Example() {
+    XCTAssertEqual(day11_2(exampleInput), 0)
+  }
+
+  func testPart2() {
+    XCTAssertEqual(day11_2(input), 0)
+  }
+}

31daad3d9b311bf39d30acd27f01299174c5b226
Ben Lings <ben.lings@bp.com>
2023-12-13 21:35:35 +0000

    Day 13 part 1

--- Sources/AoC2023/day13.swift
+++ Sources/AoC2023/day13.swift
@@ -1,8 +1,68 @@
 import Foundation
 import AdventCore
 
+struct MirrorPattern {
+
+  var size: Offset
+  var rocks: Set<Offset>
+
+/*
+ 012345678
+     ><
+ #.##..##.
+ ..#.##.#.
+ ##......#
+ ##......#
+ ..#.##.#.
+ ..##..##.
+ #.#.##.#.
+     ><
+ 012345678
+ */
+
+  func hasReflection(column: Int) -> Bool {
+    let range = max(0, 2 * column - size.east)..<min(size.east, 2 * column)
+    return rocks.lazy
+      .filter { range.contains($0.east) }
+      .allSatisfy { rocks.contains(Offset(east: 2 * column - $0.east - 1, north: $0.north)) }
+  }
+
+  func hasReflection(row: Int) -> Bool {
+    let range = max(0, 2 * row - size.north)..<min(size.north, 2 * row)
+    return rocks.lazy
+      .filter { range.contains($0.north) }
+      .allSatisfy { rocks.contains(Offset(east: $0.east, north: 2 * row - $0.north - 1)) }
+  }
+
+  func summary() -> Int {
+    for column in 1..<size.east {
+      if hasReflection(column: column) {
+        return column
+      }
+    }
+    for row in 1..<size.north {
+      if hasReflection(row: row) {
+        return 100 * row
+      }
+    }
+    return 0
+  }
+
+}
+
+extension MirrorPattern {
+  init(_ description: String) {
+    let grid = Grid(description, conversion: Bit.init(pixel:))
+    self.size = grid.size
+    self.rocks = grid.toSparse()
+  }
+}
+
 public func day13_1(_ input: String) -> Int {
-  0
+  input
+    .groups()
+    .map { MirrorPattern($0).summary() }
+    .sum()
 }
 
 public func day13_2(_ input: String) -> Int {
--- Tests/AoC2023Tests/day13.swift
+++ Tests/AoC2023Tests/day13.swift
@@ -6,14 +6,29 @@ final class Day13Tests: XCTestCase {
   let input = Bundle.module.text(named: "day13")
 
   let exampleInput = """
+  #.##..##.
+  ..#.##.#.
+  ##......#
+  ##......#
+  ..#.##.#.
+  ..##..##.
+  #.#.##.#.
+
+  #...##..#
+  #....#..#
+  ..##..###
+  #####.##.
+  #####.##.
+  ..##..###
+  #....#..#
   """
 
   func testPart1Example() {
-    XCTAssertEqual(day13_1(exampleInput), 0)
+    XCTAssertEqual(day13_1(exampleInput), 405)
   }
 
   func testPart1() {
-    XCTAssertEqual(day13_1(input), 0)
+    XCTAssertEqual(day13_1(input), 33780)
   }
 
   func testPart2Example() {

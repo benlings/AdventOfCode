8a89607db46fb690f73467afa415c7021c1858f6
Ben Lings <ben.lings@bp.com>
2023-12-16 20:18:24 +0000

    Day 15 part 1

--- Sources/AoC2023/day15.swift
+++ Sources/AoC2023/day15.swift
@@ -1,8 +1,20 @@
 import Foundation
 import AdventCore
 
+func calcHash(_ s: Substring) -> Int {
+  var hash = 0
+  for c in s {
+    if let i = c.asciiValue {
+      hash += Int(i)
+      hash *= 17
+      hash %= 256
+    }
+  }
+  return hash
+}
+
 public func day15_1(_ input: String) -> Int {
-  0
+  input.trimmingCharacters(in: .newlines).split(separator: ",").map(calcHash).sum()
 }
 
 public func day15_2(_ input: String) -> Int {
--- Tests/AoC2023Tests/day15.swift
+++ Tests/AoC2023Tests/day15.swift
@@ -6,14 +6,15 @@ final class Day15Tests: XCTestCase {
   let input = Bundle.module.text(named: "day15")
 
   let exampleInput = """
+  rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
   """
 
   func testPart1Example() {
-    XCTAssertEqual(day15_1(exampleInput), 0)
+    XCTAssertEqual(day15_1(exampleInput), 1320)
   }
 
   func testPart1() {
-    XCTAssertEqual(day15_1(input), 0)
+    XCTAssertEqual(day15_1(input), 516804)
   }
 
   func testPart2Example() {

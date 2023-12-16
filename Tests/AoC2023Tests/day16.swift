8378242e064ff395c66c022930101f18d15ced6b
Ben Lings <ben.lings@bp.com>
2023-12-16 22:04:43 +0000

    Scaffold Day 16

---
+++ Tests/AoC2023Tests/day16.swift
@@ -0,0 +1,26 @@
+import XCTest
+import AoC2023
+
+final class Day16Tests: XCTestCase {
+
+  let input = Bundle.module.text(named: "day16")
+
+  let exampleInput = """
+  """
+
+  func testPart1Example() {
+    XCTAssertEqual(day16_1(exampleInput), 0)
+  }
+
+  func testPart1() {
+    XCTAssertEqual(day16_1(input), 0)
+  }
+
+  func testPart2Example() {
+    XCTAssertEqual(day16_2(exampleInput), 0)
+  }
+
+  func testPart2() {
+    XCTAssertEqual(day16_2(input), 0)
+  }
+}

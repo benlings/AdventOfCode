1803bd0f2326a7d1542820c4d0c81101648747f9
Ben Lings <ben.lings@bp.com>
2023-12-17 23:25:08 +0000

    Create struct

--- Sources/AoC2023/day14.swift
+++ Sources/AoC2023/day14.swift
@@ -9,19 +9,18 @@ enum Rock: Character, CustomStringConvertible {
   case space = "."
 }
 
-public func day14_1(_ input: String) -> Int {
-  var grid = Grid(input, conversion: Rock.init)
-  for n in grid.rowIndices {
-    if n == 0 { continue }
-    for e in grid.columnIndices {
-      let start = Offset(east: e, north: n)
+struct ReflectorDish {
+  var grid: Grid<Rock>
+
+  mutating func tilt() {
+    for start in grid.range() {
       let rock = grid[start]
       if rock == .rounded {
         var pos = start
         repeat {
           pos += .south
         } while grid.contains(pos) && grid[pos] == .space
-        pos += .north
+        pos -= .south
         if pos != start {
           grid[start] = .space
           grid[pos] = .rounded
@@ -29,10 +28,25 @@ public func day14_1(_ input: String) -> Int {
       }
     }
   }
-  let rowCount = grid.size.north
-  return grid.rowIndices.map { row in
-    (rowCount - row) * grid.row(row).count { $0 == .rounded }
-  }.sum()
+
+  var northSupportLoad: Int {
+    let rowCount = grid.size.north
+    return grid.rowIndices.map { row in
+      (rowCount - row) * grid.row(row).count { $0 == .rounded }
+    }.sum()
+  }
+}
+
+extension ReflectorDish {
+  init(_ description: String) {
+    self.grid = Grid(description, conversion: Rock.init)
+  }
+}
+
+public func day14_1(_ input: String) -> Int {
+  var dish = ReflectorDish(input)
+  dish.tilt()
+  return dish.northSupportLoad
 }
 
 public func day14_2(_ input: String) -> Int {

8332d28e8c2bef2ebb2c52e359359798911df5f7
Ben Lings <ben.lings@bp.com>
2023-12-17 20:37:17 +0000

    Day 17 part 1

--- Sources/AoC2023/day17.swift
+++ Sources/AoC2023/day17.swift
@@ -1,8 +1,62 @@
 import Foundation
 import AdventCore
 
+fileprivate struct State: Hashable {
+  var position: Offset = .zero
+  var direction: Offset
+  var moves: Int = 0
+
+  func next(angle: Int) -> State {
+    var copy = self
+    if angle == 0 {
+      copy.moves += 1
+    } else {
+      copy.moves = 0
+    }
+    copy.direction.rotate(angle: angle)
+    copy.position += copy.direction
+    return copy
+  }
+
+  func neighbours() -> [State] {
+    var neighbours = [next(angle: -90), next(angle: 90)]
+    if moves < 2 {
+      neighbours.append(next(angle: 0))
+    }
+    return neighbours
+  }
+}
+
+fileprivate func findShortestPath(start: [State], end: Offset, cost: (State, State) -> Int?) -> Int? {
+  var costs = [State: Int]()
+  var toVisit = PriorityQueue<State, Int>()
+  for s in start {
+    costs[s] = 0
+    toVisit.insert(s, priority: 0)
+  }
+  while let current = toVisit.popMin() {
+    if current.position == end {
+      return costs[current]
+    }
+    let currentCost = costs[current, default: .max]
+    for neighbour in current.neighbours() {
+      guard let neighbourCost = cost(current, neighbour) else { continue }
+      let alt = currentCost + neighbourCost
+      if alt < costs[neighbour, default: .max] {
+        costs[neighbour] = alt
+        toVisit.insert(neighbour, priority: alt)
+      }
+    }
+  }
+  return nil
+}
+
 public func day17_1(_ input: String) -> Int {
-  0
+  let grid = Grid(input) { $0.wholeNumberValue }
+  let end = grid.size - Offset(east: 1, north: 1)
+  return findShortestPath(start: [State(direction: .east), State(direction: .north)],
+                          end: end,
+                          cost: { grid.contains($1.position) ? grid[$1.position] : nil })!
 }
 
 public func day17_2(_ input: String) -> Int {
--- Tests/AoC2023Tests/day17.swift
+++ Tests/AoC2023Tests/day17.swift
@@ -6,14 +6,27 @@ final class Day17Tests: XCTestCase {
   let input = Bundle.module.text(named: "day17")
 
   let exampleInput = """
+  2413432311323
+  3215453535623
+  3255245654254
+  3446585845452
+  4546657867536
+  1438598798454
+  4457876987766
+  3637877979653
+  4654967986887
+  4564679986453
+  1224686865563
+  2546548887735
+  4322674655533
   """
 
   func testPart1Example() {
-    XCTAssertEqual(day17_1(exampleInput), 0)
+    XCTAssertEqual(day17_1(exampleInput), 102)
   }
 
   func testPart1() {
-    XCTAssertEqual(day17_1(input), 0)
+    XCTAssertEqual(day17_1(input), 1256)
   }
 
   func testPart2Example() {

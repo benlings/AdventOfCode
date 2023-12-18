3cf61fc52e5ca4ff3baaa829f21ad0ca2bbc764f
Ben Lings <ben.lings@bp.com>
2023-12-18 22:00:03 +0000

    Parse part 2 input

--- Sources/AoC2023/day18.swift
+++ Sources/AoC2023/day18.swift
@@ -11,6 +11,15 @@ extension Offset {
     default: return nil
     }
   }
+  init?(digDirectionHex: Character) {
+    switch digDirectionHex {
+    case "0": self = .east
+    case "2": self = .west
+    case "3": self = .south
+    case "1": self = .north
+    default: return nil
+    }
+  }
 }
 
 public func day18_1(_ input: String) -> Int {
@@ -42,5 +51,27 @@ public func day18_1(_ input: String) -> Int {
 }
 
 public func day18_2(_ input: String) -> Int {
-  0
+  let instructions = input.lines().compactMap { line -> Offset? in
+    let s = Scanner(string: line)
+    guard let _ = s.scanCharacter(),
+          let _ = s.scanInt(),
+          let _ = s.scanString("(#"),
+          let h = s.scanUpToString(")"),
+          let dist = Int(h.prefix(5), radix: 16),
+          let c = h.dropFirst(5).first,
+          let direction = Offset(digDirectionHex: c)
+    else { return nil }
+    return dist * direction
+  }
+  var start = Offset.zero
+  var edge = Set<Offset>()
+  for i in instructions {
+    let end = start + i
+    let line = Line(start: start, end: end)
+    edge.formUnion(line.points)
+    start = end
+  }
+  print(edge.topLeft)
+  print(edge.bottomRight)
+  return edge.count
 }

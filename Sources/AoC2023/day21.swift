38bdde5eb411abd97e80757934df5f1f75a17b08
Ben Lings <ben.lings@bp.com>
2023-12-21 19:43:19 +0000

    Day 21 part 1

--- Sources/AoC2023/day21.swift
+++ Sources/AoC2023/day21.swift
@@ -1,8 +1,22 @@
 import Foundation
 import AdventCore
 
-public func day21_1(_ input: String) -> Int {
-  0
+public func day21_1(_ input: String, steps: Int = 64) -> Int {
+  var map = Grid(input, conversion: { $0 })
+  guard let start = map.firstIndex(of: "S") else { return 0 }
+  var positions = Set([start])
+  for _ in 0..<steps {
+    var nextPositions = Set<Offset>()
+    for p in positions {
+      for n in p.orthoNeighbours() {
+        if map.contains(n) && map[n] != "#" {
+          nextPositions.insert(n)
+        }
+      }
+    }
+    positions = nextPositions
+  }
+  return positions.count
 }
 
 public func day21_2(_ input: String) -> Int {
--- Tests/AoC2023Tests/day21.swift
+++ Tests/AoC2023Tests/day21.swift
@@ -6,14 +6,25 @@ final class Day21Tests: XCTestCase {
   let input = Bundle.module.text(named: "day21")
 
   let exampleInput = """
+  ...........
+  .....###.#.
+  .###.##..#.
+  ..#.#...#..
+  ....#.#....
+  .##..S####.
+  .##..#...#.
+  .......##..
+  .##.#.####.
+  .##..##.##.
+  ...........
   """
 
   func testPart1Example() {
-    XCTAssertEqual(day21_1(exampleInput), 0)
+    XCTAssertEqual(day21_1(exampleInput, steps: 6), 16)
   }
 
   func testPart1() {
-    XCTAssertEqual(day21_1(input), 0)
+    XCTAssertEqual(day21_1(input), 3729)
   }
 
   func testPart2Example() {
